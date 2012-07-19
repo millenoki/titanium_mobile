4e21821a705350a39192848ebeefc19dcbe15155
Martin Guillon <martin.guillon@gmail.com>
2012-07-19 09:39:20 +0200

    changes to HTTPClient. Renamed ondatastream to ondownloadprogress. Renamed onsendstream to onsendprogress added ondatastream = for continuous data received like comet session
    Conflicts:
    	iphone/Classes/TiNetworkHTTPClientProxy.h
    	iphone/Classes/TiNetworkHTTPClientProxy.m

--- iphone/Classes/TiNetworkHTTPClientProxy.h
+++ iphone/Classes/TiNetworkHTTPClientProxy.h
@@ -1,10 +1,8 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
- * 
- * WARNING: This is generated code. Modify at your own risk and without support.
  */
 #ifdef USE_TI_NETWORK
 
@@ -15,37 +13,37 @@
 #import "ASIProgressDelegate.h"
 
 typedef enum {
-    NetworkClientStateUnsent = 0,
-    NetworkClientStateOpened = 1,
-    NetworkClientStateHeaders = 2,
-    NetworkClientStateLoading = 3,
-    NetworkClientStateDone = 4, 
+	NetworkClientStateUnsent = 0,
+	NetworkClientStateOpened = 1,
+	NetworkClientStateHeaders = 2,
+	NetworkClientStateLoading = 3,
+	NetworkClientStateDone = 4,	
 } NetworkClientState;
 
 
-@interface TiNetworkHTTPClientProxy : TiProxy<TI_ASIHTTPRequestDelegate,TI_ASIProgressDelegate> 
+@interface TiNetworkHTTPClientProxy : TiProxy<ASIHTTPRequestDelegate,ASIProgressDelegate> 
 {
 @private
-    ASIFormDataRequest *request;
-    NetworkClientState readyState;
-    BOOL connected;
-    BOOL async;
-    NSURL *url;
-    long long uploadProgress;
-    long long downloadProgress;
-    long long downloadLength;
-    long long uploadLength;
-    NSNumber* validatesSecureCertificate;
+	ASIFormDataRequest *request;
+	NetworkClientState readyState;
+	BOOL connected;
+	BOOL async;
+	NSURL *url;
+	long long uploadProgress;
+	long long downloadProgress;
+	long long downloadLength;
+	long long uploadLength;
+	NSNumber* validatesSecureCertificate;
     NSNumber* timeout;
     NSNumber* autoRedirect;
-    
-    // callbacks are now in the JS object
-    BOOL hasOnload;
-    BOOL hasOnerror;
-    BOOL hasOnreadystatechange;
-    BOOL hasOndatastream;
-    BOOL hasOnsendprogress;
-    BOOL hasOndownloadgrogress;
+	
+	// callbacks are now in the JS object
+	BOOL hasOnload;
+	BOOL hasOnerror;
+	BOOL hasOnreadystatechange;
+	BOOL hasOndatastream;
+	BOOL hasOnsendprogress;
+	BOOL hasOndownloadgrogress;
 }
 // Internal
 -(NSDictionary*)responseHeaders;
@@ -63,8 +61,8 @@ typedef enum {
 @property(nonatomic,readonly) BOOL connected;
 @property(nonatomic,readonly) NSInteger readyState;
 @property(nonatomic,readonly) NSString* responseText;
-@property(nonatomic,readonly) TiProxy* responseXML; 
-@property(nonatomic,readonly) TiBlob* responseData; 
+@property(nonatomic,readonly) TiProxy* responseXML;	
+@property(nonatomic,readonly) TiBlob* responseData;	
 @property(nonatomic,readonly) NSString* connectionType;
 @property(nonatomic,readonly) NSString* location;
 @property(nonatomic,retain,readwrite) NSNumber* validatesSecureCertificate;
