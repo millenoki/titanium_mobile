c548011faae0131d3e65f1352af51537ab83f435
Martin Guillon <martin.guillon@gmail.com>
2012-11-13 17:36:02 +0100

    I don't like people to know what i am doing!

--- iphone/cli/commands/_build.js
+++ iphone/cli/commands/_build.js
@@ -539,28 +539,28 @@ exports.run = function (logger, config, cli, finished) {
 function sendAnalytics(cli) {
 	var eventName = cli.argv['device-family'] + '.' + cli.argv.target;
 
-	if (cli.argv.target == 'dist-appstore' || cli.argv.target == 'dist-adhoc') {
-		eventName = cli.argv['device-family'] + '.distribute.' + cli.argv.target.replace('dist-', '');
-	} else if (cli.argv['debug-host']) {
-		eventName += '.debug';
-	} else {
-		eventName += '.run';
-	}
+	// if (cli.argv.target == 'dist-appstore' || cli.argv.target == 'dist-adhoc') {
+	// 	eventName = cli.argv['device-family'] + '.distribute.' + cli.argv.target.replace('dist-', '');
+	// } else if (cli.argv['debug-host']) {
+	// 	eventName += '.debug';
+	// } else {
+	// 	eventName += '.run';
+	// }
 
-	cli.addAnalyticsEvent(eventName, {
-		dir: cli.argv['project-dir'],
-		name: cli.tiapp.name,
-		publisher: cli.tiapp.publisher,
-		url: cli.tiapp.url,
-		image: cli.tiapp.image,
-		appid: cli.tiapp.id,
-		description: cli.tiapp.description,
-		type: cli.argv.type,
-		guid: cli.tiapp.guid,
-		version: cli.tiapp.version,
-		copyright: cli.tiapp.copyright,
-		date: (new Date()).toDateString()
-	});
+	// cli.addAnalyticsEvent(eventName, {
+	// 	dir: cli.argv['project-dir'],
+	// 	name: cli.tiapp.name,
+	// 	publisher: cli.tiapp.publisher,
+	// 	url: cli.tiapp.url,
+	// 	image: cli.tiapp.image,
+	// 	appid: cli.tiapp.id,
+	// 	description: cli.tiapp.description,
+	// 	type: cli.argv.type,
+	// 	guid: cli.tiapp.guid,
+	// 	version: cli.tiapp.version,
+	// 	copyright: cli.tiapp.copyright,
+	// 	date: (new Date()).toDateString()
+	// });
 }
 
 function build(logger, config, cli, finished) {
