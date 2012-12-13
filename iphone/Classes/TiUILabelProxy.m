875c05cf77d37606baf721f5563ae538b64062f1
Martin Guillon <martin.guillon@gmail.com>
2012-12-13 12:05:55 +0100

    fixed contentHeightForWidth computation for label

--- iphone/Classes/TiUILabelProxy.m
+++ iphone/Classes/TiUILabelProxy.m
@@ -21,20 +21,7 @@ USE_VIEW_FOR_CONTENT_WIDTH
 
 -(CGFloat)contentHeightForWidth:(CGFloat)suggestedWidth
 {
-	NSString *value = [TiUtils stringValue:[self valueForKey:@"text"]];
-	id fontValue = [self valueForKey:@"font"];
-	UIFont *font = nil;
-	if (fontValue!=nil)
-	{
-		font = [[TiUtils fontValue:[self valueForKey:@"font"]] font];
-	}
-	else 
-	{
-		font = [UIFont systemFontOfSize:[UIFont labelFontSize]];
-	}
-	CGSize maxSize = CGSizeMake(suggestedWidth<=0 ? 480 : suggestedWidth, 10000);
-	CGSize size = [value sizeWithFont:font constrainedToSize:maxSize lineBreakMode:UILineBreakModeTailTruncation];
-	return [self verifyHeight:size.height]; //Todo: We need to verifyHeight elsewhere as well.
+	return [[self view] contentHeightForWidth:suggestedWidth]; 
 }
 
 -(CGFloat) verifyWidth:(CGFloat)suggestedWidth
