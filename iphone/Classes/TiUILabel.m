f84b75ece7b87b93dd854119a34933363dcbb8aa
Martin Guillon <martin.guillon@gmail.com>
2013-01-18 14:50:05 +0100

    I am not sure why but using systemFont wasn't working on retina devices. Using the font name directly is quite hugly but at least it works :s

--- iphone/Classes/TiUILabel.m
+++ iphone/Classes/TiUILabel.m
@@ -190,7 +190,7 @@ static inline CTLineBreakMode UILineBreakModeToCTLineBreakMode(UILineBreakMode l
             if (webFont.family)
                 [options setValue:webFont.family forKey:DTDefaultFontFamily];
             else
-                [options setValue:[UIFont systemFontOfSize:12].fontName forKey:DTDefaultFontFamily];
+                [options setValue:@"Helvetica" forKey:DTDefaultFontFamily];
 
             [options setValue:[NSNumber numberWithFloat:(webFont.size / kDefaultFontSize)] forKey:NSTextSizeMultiplierDocumentOption];
             
