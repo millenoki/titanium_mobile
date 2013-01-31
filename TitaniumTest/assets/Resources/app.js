610979d31594260f3ddd85e0cbd1cdcc3e77e79a
Martin Guillon <martin.guillon@gmail.com>
2013-01-31 10:18:10 +0100

    work on the dev sample

--- TitaniumTest/assets/Resources/app.js
+++ TitaniumTest/assets/Resources/app.js
@@ -1,273 +1,156 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
-
-
-// open a single window
-var win = Ti.UI.createWindow({
-    backgroundColor:'gray'
-});
-win.barColor = '#385292';
-
-//
-// CREATE SEARCH BAR
-//
-var search = Titanium.UI.createSearchBar({
-	barColor:'#385292',
-	showCancel:false
-});
-search.addEventListener('change', function(e)
-{
-	e.value; // search string as user types
-});
-search.addEventListener('return', function(e)
-{
-	search.blur();
-});
-search.addEventListener('cancel', function(e)
-{
-	search.blur();
-});
-
-var tableView;
-var data = [];
-var menuWidth = 200;
-
-// create first row
-//var row = Ti.UI.createTableViewRow();
-//row.backgroundColor = '#576996';
-//row.selectedBackgroundColor = '#385292';
-//row.height = 40;
-//var clickLabel = Titanium.UI.createLabel({
-//	text:'Click different parts of the row',
-//	color:'#fff',
-//	textAlign:'center',
-//	font:{fontSize:14},
-//	width:'auto',
-//	height:'auto'
-//});
-//row.className = 'header';
-//row.add(clickLabel);
-//data.push(row);
-
-// when you click the header, scroll to the bottom
-//row.addEventListener('click',function()
-//{
-//	tableView.scrollToIndex(40,{animated:true,position:Ti.UI.iPhone.TableViewScrollPosition.TOP});
-//});
-
-// create update row (used when the user clicks on the row)
-function createUpdateRow(text)
-{
-	var updateRow = Ti.UI.createTableViewRow();
-	updateRow.backgroundColor = '#13386c';
-	updateRow.selectedBackgroundColor = 'transparent';
-
-	// add custom property to identify this row
-	updateRow.isUpdateRow = true;
-	var updateRowText = Ti.UI.createLabel({
-		color:'#fff',
-		font:{fontSize:20, fontWeight:'bold'},
-		text:text,
-		width:'auto',
-		height:'auto'
-	});
-	updateRow.className = 'updated_row';
-	updateRow.add(updateRowText);
-	return updateRow;
-}
-// create a var to track the active row
-var currentRow = null;
-var currentRowIndex = null;
-
-// create the rest of the rows
-for (var c=1;c<50;c++)
-{
-	var row = Ti.UI.createTableViewRow({
-		selectionStyle: Titanium.UI.iPhone.TableViewCellSelectionStyle.NONE,
-		backgroundColor:"#222"
+var win = Ti.UI.createWindow({backgroundColor:'gray'});
+var button1 = Titanium.UI.createButton({
+	  left:10,
+	  top:10,
+	  height:50,
+	  width:50,
+	  enabled:true,
+	  focusable:true,
+	  title:'B1',
+	  backgroundColor: 'white',
+	  backgroundSelectedColor: 'yellow',
+	  backgroundImage:'../images/slightlylargerimage.png'
 	});
 
-	row.selectedBackgroundColor = '#fff';
-	row.height = 100;
-	row.className = 'datarow';
-	row.clickName = 'row';
+	var button2 = Titanium.UI.createButton({
+	  left:70,
+	  top:10,
+	  height:50,
+	  width:50,
+	  focusable:true,
+	  enabled:true,
+	  title:'B2',
+	  backgroundColor: 'white',
+	  backgroundDisabledColor:'orange',
+	  backgroundDisabledImage:'../images/slightlylargerimage.png'
+	});
 
-	row.container = Ti.UI.createView({
-		left:0,
-		right:0,
-		top:0,
-		bottom:0,
-		backgroundColor:"#666"
+	var button3 = Titanium.UI.createButton({
+	  left:130,
+	  top:10,
+	  height:50,
+	  width:50,
+	  enabled:true,
+	  focusable:true,
+	  title:'B3',
+	  backgroundColor: 'red',
+	  backgroundSelectedColor: 'blue',
+	  backgroundFocusedImage:'../images/slightlylargerimage.png'
+	});
+
+	var button4 = Titanium.UI.createButton({
+	  left:190,
+	  top:10,
+	  height:50,
+	  width:50,
+	  focusable:true,
+	  enabled:true,
+	  title:'B4',
+	  backgroundColor: 'white',
+	  backgroundSelectedImage:'../images/slightlylargerimage.png'
+	});
+
+	var button5 = Titanium.UI.createButton({
+	  left:10,
+	  top:200,
+	  height:60,
+	  width:100,
+	  focusable:true,
+	  enabled:true,
+	  title:'click me'
+	});
+
+	var state=1;
+	button5.addEventListener('click',function()
+	{
+		switch(state)
+		{
+			case 0:
+				button2.focusable=true;
+				button2.enabled=true;
+				state=1;
+				break;
+			case 1:
+				button2.focusable=false;
+				button2.enabled=false;
+				state=0;
+				break;
+			
+		}
 	})
 
-	var photo = Ti.UI.createView({
-		backgroundImage:'/images/custom_tableview/user.png',
-		top:5,
-		left:10,
-		width:50,
-		height:50,
-		clickName:'photo'
-	});
-	row.container.add(photo);
 
-	var text = 'Fred Smith '+c;
-	var user = Ti.UI.createLabel({
-		color:'white',
-		font:{fontSize:16,fontWeight:'bold', fontFamily:'Arial'},
-		left:70,
-		top:2,
-		height:30,
-		width:200,
-		clickName:'user',
-		text:text
-	});
-
-	row.name = text;
-	row.container.add(user);
-
-	var fontSize = 16;
-	if (Titanium.Platform.name == 'android') {
-		fontSize = 14;
-	}
-	var comment = Ti.UI.createLabel({
-		color:'silver',
-		font:{fontSize:fontSize,fontWeight:'normal', fontFamily:'Arial'},
-		left:70,
-		top:21,
-		height:50,
-		width:200,
-		clickName:'comment',
-		text:'Got some fresh fruit, conducted some business, took a nap'
-	});
-	row.container.add(comment);
-
-	var calendar = Ti.UI.createView({
-		backgroundImage:'/images/custom_tableview/eventsButton.png',
-		bottom:2,
-		left:70,
-		width:32,
-		clickName:'calendar',
-		height:32
-	});
-	row.container.add(calendar);
-
-	var button = Ti.UI.createView({
-		backgroundImage:'/images/custom_tableview/commentButton.png',
-		top:35,
-		right:5,
-		width:36,
-		clickName:'button',
-		height:34
-	});
-	row.container.add(button);
-
-	var date = Ti.UI.createLabel({
-		color:'#999',
-		font:{fontSize:13,fontWeight:'normal', fontFamily:'Arial'},
-		left:105,
-		bottom:5,
-		height:20,
-		width:100,
-		clickName:'date',
-		text:'posted on 3/11'
-	});
-	row.container.add(date);
-
-	row.add(row.container);
-	data.push(row);
-}
-
-
-//
-// create table view (
-//
-tableView = Titanium.UI.createTableView({
-	data:data,
-	search:search,
-	filterAttribute:'name',
-	backgroundColor:'white'
-});
-
-var currentRowMenu = null;
-var currentMenu = null;
-
-function hideMenu()
-{
-	if (currentRowMenu!=null)
-	{
-		var oldMenu = currentMenu;
-		var oldRowMenu = currentRowMenu;
-		oldRowMenu.container.animate({left:0, duration:200});
-		oldMenu.animate({left:-menuWidth, duration:200}, function(){
-			oldRowMenu.container.left = 0;
-			oldRowMenu.remove(oldMenu);
-			oldMenu = null;
-			oldRowMenu = null;
-		});
-	}
-}
-
-function showMenuForRow(_row)
-{
-	hideMenu();
-
-	var menu = Ti.UI.createView({
-		width:menuWidth,
-		layout:'horizontal',
-		horizontalWrap:false,
-		left:-menuWidth,
-		top:0,
-		bottom:0,
-		height:Ti.UI.FILL,
-		backgroundColor:"#3ff"
-	})
-	menu.add(Ti.UI.createView({
-		width:Ti.UI.FILL
-	}));
-	menu.add(Ti.UI.createButton({
-		title:'1',
-		right:10
-	}));
-	menu.add(Ti.UI.createButton({
-		title:'2',
-		right:10
-	}));
-	menu.add(Ti.UI.createButton({
-		title:'3',
-		right:10
-	}));
-	_row.add(menu);
-	menu.animate({left:0, duration:200});
-	_row.container.animate({left:menuWidth, duration:200});
-	currentRowMenu = _row;
-	currentMenu = menu;
-	menu = null;
-}
-
-tableView.addEventListener('swipe', function(e)
-{
-	if(e.row && e.direction == 'right')
-	{
-		if (e.row.isUpdateRow) return;
-		showMenuForRow(e.row);
-	}
-});
-
-
-//tableView.addEventListener('singletap', function(e)
-//{
-//	if (!e.row) return;
-//	if (e.source.toString() == '[object TiUIButton]') return;
-//	Ti.API.info('table view row clicked - source ' + e.source);
-//	if (e.row.isUpdateRow) return;
-//	// use rowNum property on object to get row number
-//	var rowNum = e.index;
-//	var updateRow = createUpdateRow('You clicked on the '+e.source.clickName);
-//	tableView.updateRow(rowNum,updateRow,{animationStyle:Titanium.UI.iPhone.RowAnimationStyle.LEFT});
-//});
-
-win.add(tableView);
+	win.add(button1);
+	win.add(button2);
+	win.add(button3);
+	win.add(button4);
+	win.add(button5);
+	
+	var view1 = Titanium.UI.createView({
+		  height:200,
+		  width:200,
+		  borderRadius:30,
+		  borderWidth:10,
+		  backgroundColor: 'orange',
+		  backgroundSelectedColor:'yellow'
+		})
+	
+		var view2 = Titanium.UI.createView({
+			left:-50,
+			top:-50,
+		  height:100,
+		  width:100,
+		  backgroundColor: 'blue'
+		})
+	view1.add(view2);
+	win.add(view1);
+	
+	win.add(Titanium.UI.createView({
+		  left:10,
+		  bottom:10,
+		  height:50,
+		  width:50,
+		  borderColor:'white',
+		  backgroundColor: 'silver',
+		  backgroundSelectedColor:'white'
+		}));
+	
+	win.add(Titanium.UI.createView({
+		  left:70,
+		  bottom:10,
+		  height:50,
+		  width:50,
+		  borderRadius:8,
+		  backgroundColor: 'silver',
+		  backgroundSelectedColor:'yellow'
+		}));
+	
+	win.add(Titanium.UI.createView({
+		  left:130,
+		  bottom:10,
+		  height:50,
+		  width:50,
+		  borderColor:'blue',
+		  borderRadius:20,
+		  borderWidth:3,
+		  backgroundColor: 'silver',
+		  backgroundSelectedColor:'yellow'
+		}));
+	
+	win.add(Titanium.UI.createView({
+		  left:190,
+		  bottom:10,
+		  height:50,
+		  width:50,
+		  borderColor:'blue',
+		  borderRadius:20,
+		  backgroundColor: 'silver',
+		  backgroundGradient: {
+		        type: 'linear',
+		        startPoint: { x: '0%', y: '50%' },
+		        endPoint: { x: '100%', y: '50%' },
+		        colors: [ { color: 'red', offset: 0.0}, { color: 'blue', offset: 0.25 }, { color: 'red', offset: 1.0 } ],
+		    }
+		}));
 
 win.open();
