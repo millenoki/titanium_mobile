c4bf9adcb87addcfcf39ca6c2387b958c81a2b1c
Martin Guillon <martin.guillon@gmail.com>
2013-02-20 20:21:38 +0100

    I dont think the MIN was correct. We should return the max computed size for autoHeight

--- iphone/Classes/TiViewProxy.m
+++ iphone/Classes/TiViewProxy.m
@@ -2670,7 +2670,7 @@ if(OSAtomicTestAndSetBarrier(flagBit, &dirtyflags))	\
         }
 
         //Ensure that autoHeightForSize is called with the lowest limiting bound
-        CGFloat desiredWidth = MIN([child minimumParentWidthForSize:bounds.size],bounds.size.width);
+        CGFloat desiredWidth = MAX([child minimumParentWidthForSize:bounds.size],bounds.size.width);
 
         //TOP + BOTTOM
         CGFloat offsetV = TiDimensionCalculateValue([child layoutProperties]->top, bounds.size.height)
