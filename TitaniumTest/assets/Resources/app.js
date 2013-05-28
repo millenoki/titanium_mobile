7be17e01d1ed02e6facb83fe08b5e9620428a32c
Martin Guillon <martin.guillon@gmail.com>
2013-05-28 11:04:59 +0200

    test version for transform animation

--- TitaniumTest/assets/Resources/app.js
+++ TitaniumTest/assets/Resources/app.js
@@ -1,44 +1,120 @@
 
-var wasStartedAtBoot = Ti.App.Android.wasStartedAtBoot;
-Ti.API.info('test0: ' + wasStartedAtBoot);
-
 var win = Ti.UI.createWindow({
 	backgroundColor:'white',
-	modal:false,
 	exitOnClose:true,
 	title:'TestAutoBoot'
 });
-Ti.API.info('test1');
 
-var bc = Ti.Android.createBroadcastReceiver({
-	onReceived: function(e) {
-		Ti.API.info(JSON.stringify(e));
-	}
+var imgpath = '/images/poster.jpg';
+
+var identity = Ti.UI.create2DMatrix();
+var transformed = identity.translate(-180, -80).scale(0.2, 0.2);
+
+var isTransformed = true;
+
+var imageview = Ti.UI.createImageView({
+	top:0,
+	left:0,
+	right:0,
+	bottom:0,
+	scaleType:Ti.UI.SCALE_TYPE_ASPECT_FILL,
+	image:imgpath,
+	transform: transformed
 });
-Ti.Android.registerBroadcastReceiver(bc, [Ti.Android.ACTION_AIRPLANE_MODE_CHANGED,Ti.Android.ACTION_BOOT_COMPLETED]);
+win.add(imageview);
 
-Ti.API.info('test2');
-if (wasStartedAtBoot) {
-	function onResume (){
-		Titanium.Android.currentActivity.removeEventListener("resume", onResume);
-		Titanium.Android.currentActivity.removeEventListener("resumed", onResume);
-		win.open();
+function createFullscreenImageWindow(_args) {
+	var self = Ti.UI.createWindow(_args);
+	var initRect = null;
+	var initTransform = null;
+
+
+	var imageWidget = Ti.UI.createImageView({
+        image: _args.image,
+		scaleType:Ti.UI.SCALE_TYPE_ASPECT_FIT,
+	    localLoadSync:true,
+	    backgroundColor:'black',
+	    opacity:0,
+		top:0,
+		left:0,
+		right:0,
+		bottom:0,
+		hires:true
+	});
+	if (_args.initialRect) {
+		initRect = _args.initialRect;
+		Ti.API.info('we got an init rect')
 	}
-	Titanium.Android.currentActivity.addEventListener("resume", onResume);
-	Titanium.Android.currentActivity.addEventListener("resumed", onResume);
-}
-else
-{
-//	var activity = win.getActivity();
-//	activity.invalidateOptionsMenu();
-	function onResume (){
-		Titanium.Android.currentActivity.removeEventListener("resume", onResume);
-		Titanium.Android.currentActivity.removeEventListener("resumed", onResume);
-		win.open();
+
+    self.add(imageWidget);
+	self.showAnimated = function()
+	{
+		var args = {
+			opacity:1,
+			duration:200
+		}
+		if (initRect !== null) {
+			var currentRect = imageview.rect;
+			Ti.API.info('currentRect ' + JSON.stringify(currentRect))
+			Ti.API.info('initRect ' + JSON.stringify(initRect))
+			var newCenter = {
+				x:(initRect.x + initRect.width/2),
+				y:(initRect.y + initRect.height/2)
+			};
+			var center = {
+				x:(currentRect.x + currentRect.width/2),
+				y:(currentRect.y + currentRect.height/2)
+			};
+			var scale = initRect.width/currentRect.width;
+			Ti.API.info('newCenter ' + JSON.stringify(newCenter))
+			Ti.API.info('center ' + JSON.stringify(center))
+			Ti.API.info('scale ' + scale)
+			initTransform = imageWidget.transform = Ti.UI.create2DMatrix().translate((newCenter.x - center.x), (newCenter.y - center.y)).scale(initRect.width/currentRect.width, initRect.height/currentRect.height);
+			args.transform = Ti.UI.create2DMatrix();
+		}
+//		setTimeout(function(){
+			imageWidget.animate(args);
+//		}, 10)
+	};
+
+	self.addEventListener('click', function()
+	{
+		if (initTransform !== null) {
+			imageWidget.animate({transform:initTransform, opacity:0, duration:1000}, function(){
+				self.close();
+			});
+		}
+		else {
+			self.close({opacity:0, duration:200});
+		}
+
+	});
+	
+	function onFirstLayout(){
+		self.showAnimated();
+		self.removeEventListener('postlayout', onFirstLayout);
 	}
-	Titanium.Android.currentActivity.addEventListener("resume", onResume);
-	Titanium.Android.currentActivity.addEventListener("resumed", onResume)
-	Titanium.Android.currentActivity.moveTaskToBack();
-//	activity.moveTaskToBack();
-//	win.open();
+
+	self.addEventListener('postlayout', onFirstLayout);
+    return self;
 }
+
+
+imageview.addEventListener('singletap', function(){
+	if (isTransformed)
+		imageview.animate({transform:identity, duration:200});
+//		imageview.transform = identity;
+	else
+		imageview.animate({transform:transformed, duration:200});
+	isTransformed = !isTransformed;
+//		imageview.transform = transformed;
+//	var initialRect = imageview.absoluteRect;
+//    var win = createFullscreenImageWindow({
+//    	navBarHidden:true,
+//		image : imgpath,
+//		initialRect:initialRect
+//	});
+//    win.open({animated:false});
+});
+
+win.open();
