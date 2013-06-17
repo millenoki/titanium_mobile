97b513d99ef694508ddda1d997a0706a9e40d795
Martin Guillon <martin.guillon@gmail.com>
2013-06-17 16:11:58 +0200

    This would prevent some events to be fired when touchEnabled = false like webview load

--- iphone/Classes/TiViewProxy.m
+++ iphone/Classes/TiViewProxy.m
@@ -1899,39 +1899,18 @@ LAYOUTFLAGS_SETTER(setHorizontalWrap,horizontalWrap,horizontalWrap,[self willCha
 {
 	// Note that some events (like movie 'complete') are fired after the view is removed/dealloc'd.
 	// Because of the handling below, we can safely set the view to 'nil' in this case.
-	TiUIView* proxyView = [self viewAttached] ? view : nil;
+//	TiUIView* proxyView = [self viewAttached] ? view : nil;
 	//TODO: We have to do view instead of [self view] because of a freaky race condition that can
 	//happen in the background (See bug 2809). This assumes that view == [self view], which may
 	//not always be the case in the future. Then again, we shouldn't be dealing with view in the BG...
 	
-	
-	// Have to handle the situation in which the proxy's view might be nil... like, for example,
-	// with table rows.  Automagically assume any nil view we're firing an event for is A-OK.
-    // NOTE: We want to fire postlayout events on ANY view, even those which do not allow interactions.
-	if (proxyView == nil || [proxyView interactionEnabled] || [type isEqualToString:@"postlayout"]) {
-		[super fireEvent:type withObject:obj withSource:source propagate:propagate reportSuccess:report errorCode:code message:message];
-	}
+
+    [super fireEvent:type withObject:obj withSource:source propagate:propagate reportSuccess:report errorCode:code message:message];
 }
 
 -(void)fireEvent:(NSString*)type withObject:(id)obj propagate:(BOOL)propagate reportSuccess:(BOOL)report errorCode:(int)code message:(NSString*)message;
 {
-	// Note that some events (like movie 'complete') are fired after the view is removed/dealloc'd.
-	// Because of the handling below, we can safely set the view to 'nil' in this case.
-	TiUIView* proxyView = [self viewAttached] ? view : nil;
-	//TODO: We have to do view instead of [self view] because of a freaky race condition that can
-	//happen in the background (See bug 2809). This assumes that view == [self view], which may
-	//not always be the case in the future. Then again, we shouldn't be dealing with view in the BG...
-	
-	
-	// Have to handle the situation in which the proxy's view might be nil... like, for example,
-	// with table rows.  Automagically assume any nil view we're firing an event for is A-OK.
-    // NOTE: We want to fire postlayout events on ANY view, even those which do not allow interactions.
-	if (proxyView == nil || [proxyView interactionEnabled] || [type isEqualToString:@"postlayout"]) {
-		if (eventOverrideDelegate != nil) {
-			obj = [eventOverrideDelegate overrideEventObject:obj forEvent:type fromViewProxy:self];
-		}
-		[super fireEvent:type withObject:obj propagate:propagate reportSuccess:report errorCode:code message:message];
-	}
+	[self fireEvent:type withObject:obj withSource:self propagate:propagate reportSuccess:report errorCode:code message:message];
 }
 
 
