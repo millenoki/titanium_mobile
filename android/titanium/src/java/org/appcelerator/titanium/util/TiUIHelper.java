14ab7a4a9123b22a918b0910d30f4f6c4bd5e57d
Martin Guillon <martin.guillon@gmail.com>
2013-07-08 17:52:23 +0200

    added support for negative values in getRawSize

--- android/titanium/src/java/org/appcelerator/titanium/util/TiUIHelper.java
+++ android/titanium/src/java/org/appcelerator/titanium/util/TiUIHelper.java
@@ -96,7 +96,7 @@ public class TiUIHelper
 	public static final int FACE_UP = 5;
 	public static final int FACE_DOWN = 6;
 	public static final int UNKNOWN = 7;
-	public static final Pattern SIZED_VALUE = Pattern.compile("([0-9]*\\.?[0-9]+)\\W*(px|dp|dip|sp|sip|mm|pt|in)?");
+	public static final Pattern SIZED_VALUE = Pattern.compile("(-?[0-9]*\\.?[0-9]+)\\W*(px|dp|dip|sp|sip|mm|pt|in)?");
 
 	private static Method overridePendingTransition;
 	private static Map<String, String> resourceImageKeys = Collections.synchronizedMap(new HashMap<String, String>());
