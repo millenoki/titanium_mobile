a4eca7bd97ab42ae2ab7af5b852f9d81f5d6a053
Martin Guillon <martin.guillon@gmail.com>
2013-07-17 18:58:27 +0200

    a little fix on navBarHidden to correctly handle app setting

--- android/modules/ui/src/java/ti/modules/titanium/ui/WindowProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/WindowProxy.java
@@ -295,9 +295,11 @@ public class WindowProxy extends TiWindowProxy implements TiActivityWindow
 		if (hasProperty(TiC.PROPERTY_FULLSCREEN)) {
 			intent.putExtra(TiC.PROPERTY_FULLSCREEN, TiConvert.toBoolean(getProperty(TiC.PROPERTY_FULLSCREEN), false));
 		}
-		if (hasProperty(TiC.PROPERTY_NAV_BAR_HIDDEN)) {
-			intent.putExtra(TiC.PROPERTY_NAV_BAR_HIDDEN, TiConvert.toBoolean(getProperty(TiC.PROPERTY_NAV_BAR_HIDDEN), false));
-		}
+
+		ITiAppInfo appInfo = getTiApp().getAppInfo();
+		boolean navBarHidden = TiConvert.toBoolean(getProperty(TiC.PROPERTY_NAV_BAR_HIDDEN), appInfo.isNavBarHidden());
+		getIntent().putExtra(TiC.PROPERTY_NAV_BAR_HIDDEN, navBarHidden);
+		
 		if (hasProperty(TiC.PROPERTY_WINDOW_SOFT_INPUT_MODE)) {
 			intent.putExtra(TiC.PROPERTY_WINDOW_SOFT_INPUT_MODE, TiConvert.toInt(getProperty(TiC.PROPERTY_WINDOW_SOFT_INPUT_MODE), -1));
 		}
