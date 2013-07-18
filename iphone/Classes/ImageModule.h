d21f14df0b9f9096d4a8afed092df3c3daa96ccc
Martin Guillon <martin.guillon@gmail.com>
2013-07-18 18:43:40 +0200

    first draft for iOS image module

---
+++ iphone/Classes/ImageModule.h
@@ -0,0 +1,12 @@
+
+#import "TiModule.h"
+
+#ifdef USE_TI_IMAGE
+
+@interface ImageModule : TiModule {
+
+}
+
+@end
+
+#endif
---
+++ iphone/Classes/ImageModule.m
@@ -0,0 +1,23 @@
+
+#import "TiBase.h"
+
+#ifdef USE_TI_IMAGE
+
+#import "ImageModule.h"
+
+@implementation ImageModule
+
+-(void)startup
+{
+	[super startup];
+}
+
+-(void)dealloc
+{
+	[super dealloc];
+}
+
+#pragma mark Public Constants
+@end
+
+#endif
