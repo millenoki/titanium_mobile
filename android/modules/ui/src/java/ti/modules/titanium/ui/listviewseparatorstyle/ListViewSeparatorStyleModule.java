777d09201690e70e7511ff725092375927c075d6
Martin Guillon <martin.guillon@gmail.com>
2013-07-21 13:43:08 +0200

    ListViewSeparatorStyle

---
+++ android/modules/ui/src/java/ti/modules/titanium/ui/listviewseparatorstyle/ListViewSeparatorStyleModule.java
@@ -0,0 +1,19 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package ti.modules.titanium.ui.listviewseparatorstyle;
+
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.titanium.TiContext;
+
+import ti.modules.titanium.ui.UIModule;
+import ti.modules.titanium.ui.tableviewseparatorstyle.TableViewSeparatorStyleModule;
+import ti.modules.titanium.ui.widget.TiUITableView;
+
+@Kroll.module(parentModule=UIModule.class)
+public class ListViewSeparatorStyleModule extends TableViewSeparatorStyleModule {
+}
