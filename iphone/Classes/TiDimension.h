6eae4006fbac5c3de9d8571b8a8f1bf18ffbd1f5
Martin Guillon <martin.guillon@gmail.com>
2013-08-12 14:13:14 +0200

    missing new methods for borderWidth

--- iphone/Classes/TiDimension.h
+++ iphone/Classes/TiDimension.h
@@ -196,10 +196,44 @@ TI_INLINE BOOL TiDimensionDidCalculateValue(TiDimension dimension,CGFloat boundi
 	return NO;
 }
 
+TI_INLINE BOOL TiDimensionDidCalculateValueNoPercent(TiDimension dimension, CGFloat * result)
+{
+	switch (dimension.type)
+	{
+		case TiDimensionTypeDip:
+			*result = dimension.value;
+			return YES;
+		default: {
+			break;
+		}
+	}
+	return NO;
+}
+
 TI_INLINE CGFloat TiDimensionCalculateValue(TiDimension dimension,CGFloat boundingValue)
 {
 	CGFloat result;
 	if(TiDimensionDidCalculateValue(dimension,boundingValue,&result))
+	{
+		return result;
+	}
+	return 0.0;
+}
+
+TI_INLINE CGFloat TiDimensionCalculateValueFromStringInBouding(NSString* value, CGFloat boundingValue)
+{
+	CGFloat result;
+	if(TiDimensionDidCalculateValue(TiDimensionFromObject(value),boundingValue,&result))
+	{
+		return result;
+	}
+	return 0.0;
+}
+
+TI_INLINE CGFloat TiDimensionCalculateValueFromString(NSString* value)
+{
+	CGFloat result;
+	if(TiDimensionDidCalculateValueNoPercent(TiDimensionFromObject(value),&result))
 	{
 		return result;
 	}
