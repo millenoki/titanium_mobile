8ac4e2d0d4a952ce766ecdcc7b53805c3913f6e5
Martin Guillon <martin.guillon@gmail.com>
2013-10-04 10:10:06 +0200

    new methods to capture view image with correct layer mask

--- iphone/Classes/TiUIHelper.m
+++ iphone/Classes/TiUIHelper.m
@@ -68,4 +68,72 @@
     }
     return result;
 }
+
+-(UIImage*) imageFromView:(UIView*)originalView
+{
+    //Creating a fakeView which is initialized as our original view.
+    //Actually i render images on a fakeView and take screenshot of this fakeView.
+    UIView *fakeView = [[UIView alloc] initWithFrame:originalView.frame];
+    [fakeView setBackgroundColor:originalView.backgroundColor];
+    [fakeView.layer setMasksToBounds:originalView.layer.masksToBounds];
+    [fakeView.layer setCornerRadius:originalView.layer.cornerRadius];
+    [fakeView.layer setBorderColor:originalView.layer.borderColor];
+    [fakeView.layer setBorderWidth:originalView.layer.borderWidth];
+    
+    //Getting subviews of originalView.
+    for (UIView *view in originalView.subviews)
+    {
+        //Getting screenshot of layer of view.
+        UIGraphicsBeginImageContext(view.bounds.size);
+        [view.layer renderInContext:UIGraphicsGetCurrentContext()];
+        UIImage *imageLayer = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        //If it is masked view, then get masked image.
+        if (view.layer.mask)
+        {
+            //getting screenshot of masked layer.
+            UIGraphicsBeginImageContext(view.bounds.size);
+            [view.layer.mask renderInContext:UIGraphicsGetCurrentContext()];
+            
+            //PNG Representation is most important. otherwise this masked image will not work.
+            UIImage *maskedLayerImage=[UIImage imageWithData:UIImagePNGRepresentation(UIGraphicsGetImageFromCurrentImageContext())];
+            UIGraphicsEndImageContext();
+            
+            //getting image by masking original image.
+            imageLayer = [self maskImage:imageLayer withMask:maskedLayerImage];
+        }
+        
+        //If imageLayer is pointing to a valid object, then setting this image to UIImageView, our UIImageView frame having exactly as view.frame.
+        if (imageLayer)
+        {
+            UIImageView *imageView = [[UIImageView alloc] initWithFrame:view.frame];
+            [imageView setImage:imageLayer];
+            [fakeView addSubview:imageView];
+        }
+    }
+    
+    //At the end, taking screenshot of fakeView. This will get your Original Image.
+    UIGraphicsBeginImageContext(fakeView.bounds.size);
+    [fakeView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *previewCapturedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return previewCapturedImage;
+}
+
+//Method is used to get masked image.
+- (UIImage*) maskImage:(UIImage *)image withMask:(UIImage *)maskImage
+{
+    CGImageRef maskRef = maskImage.CGImage;
+    CGImageRef mask = CGImageMaskCreate(CGImageGetWidth(maskRef),
+                                        CGImageGetHeight(maskRef),
+                                        CGImageGetBitsPerComponent(maskRef),
+                                        CGImageGetBitsPerPixel(maskRef),
+                                        CGImageGetBytesPerRow(maskRef),
+                                        CGImageGetDataProvider(maskRef), NULL, false);
+    
+    CGImageRef masked = CGImageCreateWithMask([image CGImage], mask);
+    
+    return [UIImage imageWithCGImage:masked];
+}
 @end
