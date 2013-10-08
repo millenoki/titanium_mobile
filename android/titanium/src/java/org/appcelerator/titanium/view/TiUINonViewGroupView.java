84fdb060253b909510b8c7e0c12d3bc7c50d2cee
Martin Guillon <martin.guillon@gmail.com>
2013-10-08 08:54:07 +0200

    fixed nonviewgroup for imageview

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUINonViewGroupView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUINonViewGroupView.java
@@ -12,7 +12,6 @@ public class TiUINonViewGroupView extends TiUIView {
 		super(proxy);
 	}
 
-	private FreeLayout layout;
 	private TiCompositeLayout childrenHolder;
 	@Override
 	public void add(TiUIView child, int index)
@@ -22,27 +21,7 @@ public class TiUINonViewGroupView extends TiUIView {
 			TiCompositeLayout.LayoutParams params = new TiCompositeLayout.LayoutParams();
 			params.height = ViewGroup.LayoutParams.MATCH_PARENT;
 			params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-			if (borderView != null) {
-				borderView.addView(childrenHolder, params);
-			}
-			else {
-				layout = new FreeLayout(proxy.getActivity());	
-				ViewGroup savedParent = null;
-				int savedIndex = 0;
-				if (nativeView.getParent() != null) {
-					ViewParent nativeParent = nativeView.getParent();
-					if (nativeParent instanceof ViewGroup) {
-						savedParent = (ViewGroup) nativeParent;
-						savedIndex = savedParent.indexOfChild(nativeView);
-						savedParent.removeView(nativeView);
-					}
-				}
-				layout.addView(nativeView, params);
-				layout.addView(childrenHolder, params);
-				if (savedParent != null) {
-					savedParent.addView(layout, savedIndex,getLayoutParams());
-				}
-			}
+			getOrCreateBorderView().addView(childrenHolder, params);
 			updateLayoutForChildren(proxy.getProperties());	
 		}
 		super.add(child, index);
@@ -51,18 +30,6 @@ public class TiUINonViewGroupView extends TiUIView {
 	@Override
 	public View getParentViewForChild()
 	{
-		return childrenHolder == null ? nativeView : childrenHolder;
-	}
-
-	@Override
-	public View getOuterView()
-	{
-		return borderView == null ? (layout == null ? nativeView : layout) : borderView;
-	}
-
-	@Override
-	public View getRootView()
-	{
-		return layout != null ? layout : nativeView;
+		return childrenHolder != null ? childrenHolder: nativeView;
 	}
 }
