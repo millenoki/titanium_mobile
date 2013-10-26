0553433531fe9c0a5e4b8211d9798b441c5ca1a2
Martin Guillon <martin.guillon@akylas.fr>
2013-10-26 16:51:27 +0200

    disabled libVerify!!!!!!!!!!!!!

--- iphone/Classes/TiApp.m
+++ iphone/Classes/TiApp.m
@@ -279,7 +279,7 @@ TI_INLINE void waitForMemoryPanicCleared();   //WARNING: This must never be run 
 		if (localNotification != nil) {
 			[[NSNotificationCenter defaultCenter] postNotificationName:kTiLocalNotification object:localNotification userInfo:nil];
 		}
-		TiThreadPerformOnMainThread(^{[self validator];}, YES);
+//		TiThreadPerformOnMainThread(^{[self validator];}, YES);
 	}
 }
 
