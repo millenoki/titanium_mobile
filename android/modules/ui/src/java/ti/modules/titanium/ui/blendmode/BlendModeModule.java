487214b07f42ec72b6dda1df1b6f7c9bb63097e3
Martin Guillon <martin.guillon@akylas.fr>
2013-10-27 18:02:50 +0100

    fixed crash on old androids

--- android/modules/ui/src/java/ti/modules/titanium/ui/blendmode/BlendModeModule.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/blendmode/BlendModeModule.java
@@ -4,16 +4,18 @@ import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
 
 import android.graphics.PorterDuff.Mode;
+import android.os.Build;
 
 import ti.modules.titanium.ui.UIModule;
 
 @Kroll.module(parentModule=UIModule.class)
 public class BlendModeModule extends KrollModule {
+	private static final boolean HONEYCOMB_OR_GREATER = (Build.VERSION.SDK_INT >= 11);
 
 	@Kroll.constant public static final int DARKEN = Mode.DARKEN.ordinal();
 	@Kroll.constant public static final int LIGHTEN = Mode.LIGHTEN.ordinal();
 	@Kroll.constant public static final int MULTIPLY = Mode.MULTIPLY.ordinal();
-	@Kroll.constant public static final int ADD = Mode.ADD.ordinal();
+	@Kroll.constant public static final int ADD = (HONEYCOMB_OR_GREATER?Mode.ADD.ordinal():Mode.SRC_OVER.ordinal());
 	@Kroll.constant public static final int SCREEN = Mode.SCREEN.ordinal();
 	@Kroll.constant public static final int CLEAR = Mode.CLEAR.ordinal();
 	@Kroll.constant public static final int DST = Mode.DST.ordinal();
@@ -26,6 +28,6 @@ public class BlendModeModule extends KrollModule {
 	@Kroll.constant public static final int SRC_IN = Mode.SRC_IN.ordinal();
 	@Kroll.constant public static final int SRC_OUT = Mode.SRC_OUT.ordinal();
 	@Kroll.constant public static final int SRC_OVER = Mode.SRC_OVER.ordinal();
-	@Kroll.constant public static final int OVERLAY = Mode.OVERLAY.ordinal();
+	@Kroll.constant public static final int OVERLAY = (HONEYCOMB_OR_GREATER?Mode.OVERLAY.ordinal():Mode.MULTIPLY.ordinal());
 	@Kroll.constant public static final int XOR = Mode.XOR.ordinal();
 }
