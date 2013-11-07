2524f65e897299deba0d0c68a65bb96a19f90698
Martin Guillon <martin.guillon@akylas.fr>
2013-11-07 13:53:34 +0100

    cleanup

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIButton.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIButton.java
@@ -16,11 +16,8 @@ import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.util.TiConvert;
 import org.appcelerator.titanium.util.TiUIHelper;
-import org.appcelerator.titanium.view.FreeLayout;
-import org.appcelerator.titanium.view.TiCompositeLayout;
 import org.appcelerator.titanium.view.TiDrawableReference;
 import org.appcelerator.titanium.view.TiUINonViewGroupView;
-import org.appcelerator.titanium.view.TiUIView;
 
 import android.graphics.Rect;
 import android.graphics.Color;
