79a9e0fbe5f0dd4a4d4086b715761c302112cbdf
Martin Guillon <martin.guillon@akylas.fr>
2013-11-11 19:30:16 +0100

    cleanup

--- android/titanium/src/java/org/appcelerator/titanium/transition/TransitionSwipeFade.java
+++ android/titanium/src/java/org/appcelerator/titanium/transition/TransitionSwipeFade.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.appcelerator.titanium.animation.AlphaProperty;
 import org.appcelerator.titanium.animation.TranslationProperty;
-import org.appcelerator.titanium.animation.TranslationRelativeProperty;
 import org.appcelerator.titanium.util.TiViewHelper;
 
 import android.view.View;
