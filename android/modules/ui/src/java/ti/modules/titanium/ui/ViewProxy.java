02141aa8a230c134b1228545f0fe7996cec0d896
Martin Guillon <martin.guillon@akylas.fr>
2013-12-07 17:03:22 +0100

    support properties/childTemplates in creation dict

--- android/modules/ui/src/java/ti/modules/titanium/ui/ViewProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/ViewProxy.java
@@ -8,6 +8,7 @@ package ti.modules.titanium.ui;
 
 import java.util.HashMap;
 
+import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.kroll.common.APIMap;
@@ -42,6 +43,53 @@ public class ViewProxy extends TiViewProxy {
 	public String getApiName() {
 		return "Ti.UI.View";
 	}
+	
+	public void handleCreationDict(KrollDict dict)
+	{
+		if (dict == null) {
+			return;
+		}
+		if (dict.containsKey(TiC.PROPERTY_PROPERTIES) || dict.containsKey(TiC.PROPERTY_CHILD_TEMPLATES)) {
+			super.handleCreationDict(dict.getKrollDict(TiC.PROPERTY_PROPERTIES));
+		}
+		else {
+			super.handleCreationDict(dict);
+		}
+		if (dict.containsKey(TiC.PROPERTY_CHILD_TEMPLATES)) {
+			initFromTemplate(dict, this);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected void initFromTemplate(HashMap template_,
+			KrollProxy rootProxy) {
+		if (rootProxy != null
+				&& template_.containsKey(TiC.PROPERTY_BIND_ID)) {
+			rootProxy.setProperty(
+					TiConvert.toString(template_, TiC.PROPERTY_BIND_ID),
+					this);
+		}
+		if (template_.containsKey(TiC.PROPERTY_CHILD_TEMPLATES)) {
+			Object childProperties = template_
+					.get(TiC.PROPERTY_CHILD_TEMPLATES);
+			if (childProperties instanceof Object[]) {
+				Object[] propertiesArray = (Object[]) childProperties;
+				for (int i = 0; i < propertiesArray.length; i++) {
+					Object childDict = propertiesArray[i];
+					if (childDict instanceof TiViewProxy) {
+						this.add((TiViewProxy) childDict);
+					} else {
+						TiViewProxy childProxy = createViewFromTemplate(
+								(HashMap) childDict, rootProxy);
+						if (childProxy != null){
+							this.updateKrollObjectProperties();
+							this.add(childProxy);
+						}
+					}
+				}
+			}
+		}
+	}
 
 	@SuppressWarnings("unchecked")
 	private TiViewProxy createViewFromTemplate(HashMap template_,
@@ -53,35 +101,11 @@ public class ViewProxy extends TiViewProxy {
 		try {
 			Class<? extends KrollProxy> cls = (Class<? extends KrollProxy>) Class
 					.forName(APIMap.getProxyClass(type));
-			TiViewProxy proxy = (TiViewProxy) KrollProxy.createProxy(cls, null,
+			ViewProxy proxy = (ViewProxy) KrollProxy.createProxy(cls, null,
 					new Object[] { properties }, null);
 			if (proxy == null)
 				return null;
-			proxy.updateKrollObjectProperties();
-			if (rootProxy != null
-					&& template_.containsKey(TiC.PROPERTY_BIND_ID)) {
-				rootProxy.setProperty(
-						TiConvert.toString(template_, TiC.PROPERTY_BIND_ID),
-						proxy);
-			}
-			if (template_.containsKey(TiC.PROPERTY_CHILD_TEMPLATES)) {
-				Object childProperties = template_
-						.get(TiC.PROPERTY_CHILD_TEMPLATES);
-				if (childProperties instanceof Object[]) {
-					Object[] propertiesArray = (Object[]) childProperties;
-					for (int i = 0; i < propertiesArray.length; i++) {
-						Object childDict = propertiesArray[i];
-						if (childDict instanceof TiViewProxy) {
-							proxy.add((TiViewProxy) childDict);
-						} else {
-							TiViewProxy childProxy = createViewFromTemplate(
-									(HashMap) childDict, rootProxy);
-							if (childProxy != null)
-								proxy.add(childProxy);
-						}
-					}
-				}
-			}
+			proxy.initFromTemplate(template_, rootProxy);
 			return proxy;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -106,8 +130,10 @@ public class ViewProxy extends TiViewProxy {
 		} else if (args instanceof HashMap) {
 			TiViewProxy childProxy = createViewFromTemplate((HashMap) args,
 					this);
-			if (childProxy != null)
+			if (childProxy != null) {
+				childProxy.updateKrollObjectProperties();
 				add(childProxy);
+			}
 		} else {
 			super.add(args, index);
 		}
