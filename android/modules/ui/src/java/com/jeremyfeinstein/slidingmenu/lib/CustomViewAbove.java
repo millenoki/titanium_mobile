8a3c62ff24ac375e708a9cfff439f475071b9f64
Martin Guillon <martin.guillon@akylas.fr>
2013-12-17 19:17:04 +0100

    just a little fix

--- android/modules/ui/src/java/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
+++ android/modules/ui/src/java/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
@@ -356,7 +356,7 @@ public class CustomViewAbove extends ViewGroup {
 	private boolean isInIgnoredView(MotionEvent ev) {
 		Rect rect = new Rect();
 		for (View v : mIgnoredViews) {
-			v.getHitRect(rect);
+			v.getGlobalVisibleRect(rect);
 			if (rect.contains((int)ev.getX(), (int)ev.getY())) return true;
 		}
 		return false;
