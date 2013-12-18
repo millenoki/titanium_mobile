aeb183e7ef7232e0deb3f961d25093beed14b27f
Martin Guillon <martin.guillon@akylas.fr>
2013-12-18 15:56:50 +0100

    better handling of android module template

--- support/module/android/generated/Application.mk
+++ support/module/android/generated/Application.mk
@@ -1,10 +1,13 @@
 # Main NDK build script for @MODULE_ID@
 
 APP_BUILD_SCRIPT := jni/Android.mk
-TARGET_PLATFORM := android-14
-APP_STL := stlport_shared
-#APP_ABI := armeabi armeabi-v7a x86
+TARGET_PLATFORM := android-16
+APP_ABI := armeabi armeabi-v7a x86
 
-APP_ABI := all
+APP_STL := gnustl_static
+APP_CPPFLAGS += -frtti 
+APP_CPPFLAGS += -fexceptions
+
+#APP_ABI := all
 NDK_TOOLCHAIN_VERSION=4.4.3
-APP_PLATFORM := android-14
+APP_PLATFORM := android-16
