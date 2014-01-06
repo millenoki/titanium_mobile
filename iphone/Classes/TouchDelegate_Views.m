d167bd9395466938ae7bd4b792c803bb6f529b27
Martin Guillon <martin.guillon@akylas.fr>
2014-01-06 16:38:07 +0100

    dont propaget touch event if we are not the one receiving the event

--- iphone/Classes/TouchDelegate_Views.m
+++ iphone/Classes/TouchDelegate_Views.m
@@ -14,28 +14,35 @@
 
 #pragma mark - UIResponder
 
+-(void)runSelector:(SEL)selector forTouches:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    CGPoint location = [[touches anyObject] locationInView:self];
+    UIView* view = [self hitTest:location withEvent:event];
+    if (view == self || self == view.superview)
+        [self.touchDelegate performSelector:selector withObject:touches withObject:event];
+}
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesBegan:touches withEvent:event];
-    [self.touchDelegate processTouchesBegan:touches withEvent:event];
+    [self runSelector:@selector(processTouchesBegan:withEvent:) forTouches:touches withEvent:event];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesMoved:touches withEvent:event];
-    [self.touchDelegate processTouchesMoved:touches withEvent:event];
+    [self runSelector:@selector(processTouchesMoved:withEvent:) forTouches:touches withEvent:event];
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesEnded:touches withEvent:event];
-    [self.touchDelegate processTouchesEnded:touches withEvent:event];
+    [self runSelector:@selector(processTouchesEnded:withEvent:) forTouches:touches withEvent:event];
 }
 
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesCancelled:touches withEvent:event];
-    [self.touchDelegate processTouchesCancelled:touches withEvent:event];
+    [self runSelector:@selector(processTouchesCancelled:withEvent:) forTouches:touches withEvent:event];
 }
 
 @end
