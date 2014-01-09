4ee1c40503bbbe16e7d824ca1e5dced781b2becf
Martin Guillon <martin.guillon@akylas.fr>
2014-01-09 15:00:13 +0100

    cleanup

--- android/titanium/src/java/org/appcelerator/titanium/view/TiCompositeLayout.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiCompositeLayout.java
@@ -22,7 +22,6 @@ import org.appcelerator.titanium.util.TiUIHelper;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Rect;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
