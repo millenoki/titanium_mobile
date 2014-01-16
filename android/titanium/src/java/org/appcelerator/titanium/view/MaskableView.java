85d16cecec0c9d42cb6fe441483df9a67a8b8d75
Martin Guillon <martin.guillon@akylas.fr>
2014-01-16 19:38:09 +0100

    mask working on all android versions. cant use the same technique :s

--- android/titanium/src/java/org/appcelerator/titanium/view/MaskableView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/MaskableView.java
@@ -1,6 +1,7 @@
 package org.appcelerator.titanium.view;
 
 
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
@@ -10,30 +11,55 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.Shader;
+import android.os.Build;
 
 
 public class MaskableView extends FreeLayout {
 	
+	private static final boolean HONEYCOMB_OR_GREATER = (Build.VERSION.SDK_INT >= 11);
 	
 	private Paint maskPaint;
-	Bitmap bitmap;
-	Rect bitmapRect = new Rect();
+	Bitmap originalMask;
+	Bitmap usedMask;
+	Rect lastBounds = new Rect();
 	
 	public MaskableView(Context context) {
 		super(context);
 		maskPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-	    maskPaint.setColor(0xFFFFFFFF);
-		maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+		if (HONEYCOMB_OR_GREATER)
+		{
+			maskPaint.setColor(0xFFFFFFFF);
+			maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+		}
+	}
+	
+	private void updateMask(Rect bounds){
+		if (!bounds.isEmpty() && !lastBounds.equals(bounds)) {
+			usedMask = convertToAlphaMask(originalMask, bounds);
+			lastBounds.set(bounds);
+		}
 	}
 	
 	@Override
 	public void draw(Canvas canvas)
 	{
-		if (bitmap != null) {
+		if (originalMask != null) {
 			Rect bounds = new Rect();
 			getDrawingRect(bounds);
-			super.draw(canvas);
-			canvas.drawBitmap(bitmap, bitmapRect, bounds, maskPaint);
+			if (HONEYCOMB_OR_GREATER)
+			{
+				super.draw(canvas);
+				canvas.drawBitmap(originalMask, lastBounds, bounds, maskPaint);
+			}
+			else {
+				updateMask(bounds);
+
+				Bitmap b = Bitmap.createBitmap(bounds.width(), bounds.height(), Bitmap.Config.ARGB_8888);
+				Canvas c = new Canvas(b);
+				super.draw(c);
+				maskPaint.setShader(createShader(b));
+				canvas.drawBitmap(usedMask, 0, 0, maskPaint);
+			}
 		}
 		else {
 			super.draw(canvas);
@@ -42,33 +68,53 @@ public class MaskableView extends FreeLayout {
 	@Override
 	protected void dispatchDraw(Canvas canvas)
 	{
-		if (bitmap != null && willNotDraw()) {
+		if (originalMask != null && willNotDraw()) {
 			Rect bounds = new Rect();
 			getDrawingRect(bounds);
-			super.dispatchDraw(canvas);
-			canvas.drawBitmap(bitmap, bitmapRect, bounds, maskPaint);
+			if (HONEYCOMB_OR_GREATER)
+			{
+				super.dispatchDraw(canvas);
+				canvas.drawBitmap(originalMask, lastBounds, bounds, maskPaint);
+			}
+			else {
+				updateMask(bounds);
+
+				Bitmap b = Bitmap.createBitmap(bounds.width(), bounds.height(), Bitmap.Config.ARGB_8888);
+				Canvas c = new Canvas(b);
+				super.dispatchDraw(c);
+				maskPaint.setShader(createShader(b));
+				canvas.drawBitmap(usedMask, 0, 0, maskPaint);
+			}
+			
 		}
 		else {
 			super.dispatchDraw(canvas);
 		}
 	}
 	
-	private static Bitmap convertToAlphaMask(Bitmap b) {
-		Bitmap a = Bitmap.createBitmap(b.getWidth(), b.getHeight(), Bitmap.Config.ALPHA_8);
+	private static Bitmap convertToAlphaMask(Bitmap b, Rect bounds) {
+		Bitmap a = Bitmap.createBitmap(bounds.width(), bounds.height(), Bitmap.Config.ALPHA_8);
 		Canvas c = new Canvas(a);
-		c.drawBitmap(b, 0.0f, 0.0f, null);
+		c.drawBitmap(b, new Rect(0,0, b.getWidth(), b.getHeight()), bounds, null);
 		return a;
+	}
+	
+	private static Shader createShader(Bitmap b) {
+		return new BitmapShader(b, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
 	}
 
 	public void setMask(Bitmap bitmap)
 	{
-		this.bitmap = bitmap;
 	
 		if (bitmap != null) {
-			bitmapRect.set(0,0, bitmap.getWidth(), bitmap.getHeight());
-			maskPaint.setShader(new BitmapShader(convertToAlphaMask(bitmap), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+			this.originalMask = bitmap;
+			if (HONEYCOMB_OR_GREATER){
+				lastBounds.set(0,0, bitmap.getWidth(), bitmap.getHeight());
+				maskPaint.setShader(new BitmapShader(convertToAlphaMask(originalMask, new Rect(0,0, originalMask.getWidth(), originalMask.getHeight())), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+			}
 		}
 		else {
+			this.originalMask = null;
 		}
 		invalidate();
 	}
