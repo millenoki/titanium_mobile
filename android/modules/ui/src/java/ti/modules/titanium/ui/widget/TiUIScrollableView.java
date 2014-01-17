b12a249237c1079c756480a661ae3ffcc5c12b30
Martin Guillon <martin.guillon@akylas.fr>
2014-01-17 09:16:35 +0100

    Onlyupdate current page during scroll if dragging

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIScrollableView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIScrollableView.java
@@ -58,6 +58,7 @@ public class TiUIScrollableView extends TiUIView implements  ViewPager.OnPageCha
 	boolean hardwaredDisabled = false;
 	int cacheSize = 3;
 	private Transition transition;
+	private boolean updateCurrentPageDuringScroll = true;
 	private interface IPageAdapter {
 
 		void notifyDataSetChanged();
@@ -247,11 +248,12 @@ public class TiUIScrollableView extends TiUIView implements  ViewPager.OnPageCha
 	}
 	
 	
-	private void setCurrentPageIndex(int newPage)
+	private void setCurrentPageIndex(int newPage, boolean animated)
 	{
 		if (newPage == mCurIndex) return;
 		mCurIndex = newPage;
 		proxy.setProperty(TiC.PROPERTY_CURRENT_PAGE, mCurIndex);
+		updateCacheSize();
 		((ScrollableViewProxy)proxy).firePageChange(mCurIndex, mViews.get(mCurIndex));
 	}
 	
@@ -265,11 +267,13 @@ public class TiUIScrollableView extends TiUIView implements  ViewPager.OnPageCha
 		mPager.requestDisallowInterceptTouchEvent(scrollState != ViewPager.SCROLL_STATE_IDLE);		
 		
 		if (scrollState == ViewPager.SCROLL_STATE_DRAGGING) {
+			updateCurrentPageDuringScroll = true;
 			((ScrollableViewProxy)proxy).fireScrollStart(mCurIndex, mViews.get(mCurIndex));
 		}
 		else if ((scrollState == ViewPager.SCROLL_STATE_IDLE) && isValidScroll) {
 			int oldIndex = mCurIndex;
 			{
+				updateCurrentPageDuringScroll = false;
 				((ScrollableViewProxy)proxy).fireScrollEnd(mCurIndex, mViews.get(mCurIndex));
 			}
 			if (mCurIndex >= 0) {
@@ -301,6 +305,7 @@ public class TiUIScrollableView extends TiUIView implements  ViewPager.OnPageCha
 			// `idle` and this handler is called.
 			isValidScroll = false;
 		} else if (scrollState == ViewPager.SCROLL_STATE_SETTLING) {
+			updateCurrentPageDuringScroll = false;
 			((ScrollableViewProxy)proxy).fireDragEnd(mCurIndex, mViews.get(mCurIndex));
 
 			// Note that we just fired a `dragend` so the `onPageSelected`
@@ -313,6 +318,7 @@ public class TiUIScrollableView extends TiUIView implements  ViewPager.OnPageCha
 	public void onPageSelected(int page)
 	{
 
+		updateCurrentPageDuringScroll = false;
 		// If we didn't just fire a `dragend` event then this is the case
 		// where a user drags the view and settles it on a different view.
 		// Since the OS settling logic is never run, the
@@ -358,17 +364,16 @@ public class TiUIScrollableView extends TiUIView implements  ViewPager.OnPageCha
 		// the current index. We add 0.5 so that positionFloat will be rounded
 		// half up; ie, if it has a value of 1.5, it will be rounded up to 2; if
 		// it has a value of 1.4, it will be rounded down to 1.
-		setCurrentPageIndex(Math.min(Math.max((int) Math.floor(positionFloat + 0.5), 0), mViews.size() - 1));
+		int index = Math.min(Math.max((int) Math.floor(positionFloat + 0.5), 0), mViews.size() - 1);
+		if (updateCurrentPageDuringScroll)
+		{
+			setCurrentPageIndex(index, true);
+		}
 		((ScrollableViewProxy)proxy).fireScroll(mCurIndex, positionFloat, mViews.get(mCurIndex));
 
 		// Note that we didn't just fire a `dragend`.  See the above comment
 		// in `onPageSelected`.
 		justFiredDragEnd = false;
-		
-		if (oldIndex != mCurIndex)
-		{
-			updateCacheSize();
-		}
 		
 		//Force the container to redraw on scrolling.
         //Without this the outer pages render initially and then stay static
@@ -670,9 +675,9 @@ public class TiUIScrollableView extends TiUIView implements  ViewPager.OnPageCha
 			Log.w(TAG, "Request to move to index " + index+ " ignored, as it is out-of-bounds.");
 			return;
 		}
-		if (!animated)setCurrentPageIndex(index);
-		updateCacheSize();
-		mPager.setCurrentItem(index, animated);
+		//we dont to update page during scroll but immediately. Otherwise if we jump multiple page, we will have multiple events!
+		setCurrentPageIndex(index, animated);
+		mPager.setCurrentItem(mCurIndex, animated);
 	}
 
 //	private void move(int index)
