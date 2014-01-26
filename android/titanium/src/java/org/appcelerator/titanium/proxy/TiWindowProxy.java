e6e2e19143e666fb7c7c52fa090cbec437f719ba
Martin Guillon <martin.guillon@akylas.fr>
2014-01-26 11:57:11 +0100

    we cant be opening at that time

--- android/titanium/src/java/org/appcelerator/titanium/proxy/TiWindowProxy.java
+++ android/titanium/src/java/org/appcelerator/titanium/proxy/TiWindowProxy.java
@@ -442,6 +442,7 @@ public abstract class TiWindowProxy extends TiViewProxy
 	 */
 	public void handlePostOpen()
 	{
+		opening = false;
 		if (postOpenListener != null)
 		{
 			getMainHandler().post(new Runnable() {
