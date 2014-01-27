ba1002630955c2652875c39babc635b9ea2e9551
Martin Guillon <martin.guillon@akylas.fr>
2014-01-27 11:16:18 +0100

    we need that delay on ios 7 :s

--- iphone/Classes/TiTableView.m
+++ iphone/Classes/TiTableView.m
@@ -7,6 +7,7 @@
 //
 
 #import "TiTableView.h"
+#import "TiBase.h"
 
 @implementation TiTableView
 {
@@ -20,4 +21,18 @@
     return self;
 }
 
+- (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated
+{
+    if (IOS_7) {
+        //we have to delay it on ios7 :s
+        double delayInSeconds = 0.01;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [super setContentOffset:contentOffset animated:animated];
+        });
+    }
+    else {
+        [super setContentOffset:contentOffset animated:animated];
+    }
+}
 @end
