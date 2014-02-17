7518ab5932ed8683b8ed9264f2d2fc30d2250e01
Martin Guillon <martin.guillon@akylas.fr>
2014-02-17 15:03:21 +0100

    hande orientation in global coordinates returned by touch events

--- iphone/Classes/TiUtils.m
+++ iphone/Classes/TiUtils.m
@@ -1112,35 +1112,60 @@ If the new path starts with / and the base url is app://..., we have to massage 
 	return [self tiPointValue:name properties:properties def:nil exists:NULL];
 }
 
-+(NSDictionary*)dictionaryFromTouch:(UITouch*)touch inView:(UIView*)view
+
++(NSDictionary*)dictionaryFromTouchableEvent:(id)touch inView:(UIView*)view
 {
     if (touch == nil) return [NSDictionary dictionary];
     CGPoint localPoint = [touch locationInView:view];
     CGPoint globalPoint = [touch locationInView:nil];
+    NSString* xProp = @"x";
+    NSString* yProp = @"y";
+    float xFactor = 1;
+    float yFactor = 1;
+    
+ 	UIInterfaceOrientation o = (UIInterfaceOrientation)[[UIApplication sharedApplication] statusBarOrientation];
+    
+    switch (o) {
+        case UIInterfaceOrientationPortraitUpsideDown:
+            
+            xFactor = -1;
+            yFactor = -1;
+            break;
+            
+        case UIInterfaceOrientationLandscapeLeft:
+            xProp = @"y";
+            yProp = @"x";
+            yFactor = -1;
+            break;
+            
+        case UIInterfaceOrientationLandscapeRight:
+            xProp = @"y";
+            yProp = @"x";
+            xFactor = -1;
+            break;
+        default:
+            break;
+    }
+    
     NSDictionary *evt = [NSDictionary dictionaryWithObjectsAndKeys:
-                         [NSNumber numberWithDouble:localPoint.x],@"x",
-                         [NSNumber numberWithDouble:localPoint.y],@"y",
+                         [NSNumber numberWithDouble:xFactor*localPoint.x],xProp,
+                         [NSNumber numberWithDouble:yFactor*localPoint.y],yProp,
                          [NSDictionary dictionaryWithObjectsAndKeys:
-                            [NSNumber numberWithDouble:globalPoint.x],@"x",
-                            [NSNumber numberWithDouble:globalPoint.y],@"y",
-                             nil], @"globalPoint",
+                          [NSNumber numberWithDouble:xFactor*globalPoint.x],xProp,
+                          [NSNumber numberWithDouble:yFactor*globalPoint.y],yProp,
+                          nil], @"globalPoint",
                          nil];
     return evt;
 }
 
++(NSDictionary*)dictionaryFromTouch:(UITouch*)touch inView:(UIView*)view
+{
+    return [self dictionaryFromTouchableEvent:touch inView:view];
+}
+
 +(NSDictionary*)dictionaryFromGesture:(UIGestureRecognizer*)gesture inView:(UIView*)view
 {
-    CGPoint localPoint = [gesture locationInView:view];
-    CGPoint globalPoint = [gesture locationInView:nil];
-    NSDictionary *evt = [NSDictionary dictionaryWithObjectsAndKeys:
-                         [NSNumber numberWithDouble:localPoint.x],@"x",
-                         [NSNumber numberWithDouble:localPoint.y],@"y",
-                         [NSDictionary dictionaryWithObjectsAndKeys:
-                          [NSNumber numberWithDouble:globalPoint.x],@"x",
-                          [NSNumber numberWithDouble:globalPoint.y],@"y",
-                          nil], @"globalPoint",
-                         nil];
-    return evt;
+    return [self dictionaryFromTouchableEvent:gesture inView:view];
 }
 
 +(NSDictionary*)pointToDictionary:(CGPoint)point
