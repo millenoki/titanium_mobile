a05967412830dbc1d38a5545803523cf0a16f535
Martin Guillon <martin.guillon@akylas.fr>
2014-06-10 16:06:07 +0200

    allow modules to add API maps

--- android/runtime/common/src/java/org/appcelerator/kroll/common/APIMap.java
+++ android/runtime/common/src/java/org/appcelerator/kroll/common/APIMap.java
@@ -1,21 +1,23 @@
 /** This is generated, do not edit by hand. **/
 package org.appcelerator.kroll.common;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class APIMap {
 
-    private static final Map<String, String> PROXY_MAP = createProxyMap();
+    private static Map<String, String> PROXY_MAP = createProxyMap();
 
     private static Map<String, String> createProxyMap() {
-        Map<String, String> result = new HashMap<String, String>();
-        return Collections.unmodifiableMap(result);
+        return new HashMap<String, String>();
     }
     
     public static String getProxyClass(String apiName_) {
-    	apiName_ = apiName_.replaceFirst("^(Titanium|Ti)\\.", "");
+        apiName_ = apiName_.replaceFirst("^(Titanium|Ti)\\.", "");
     	return PROXY_MAP.get(apiName_); 
+    }
+    
+    public static void addMapping(Map<String, String> map) {
+        PROXY_MAP.putAll(map);
     }
 }
