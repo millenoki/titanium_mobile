1d5316e38a3f74a2ffb8d91b03852510b5353430
Martin Guillon <martin.guillon@akylas.fr>
2014-06-18 10:41:49 +0200

    missing transitionStule

--- android/modules/ui/src/java/ti/modules/titanium/ui/transitionstyle/TransitionStyleModule.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/transitionstyle/TransitionStyleModule.java
@@ -20,7 +20,8 @@ public class TransitionStyleModule extends KrollModule {
 	@Kroll.constant public static final int PUSH_ROTATE = TransitionHelper.Types.kTransitionPushRotate.ordinal();
 	@Kroll.constant public static final int SCALE = TransitionHelper.Types.kTransitionScale.ordinal();
 	@Kroll.constant public static final int SLIDE = TransitionHelper.Types.kTransitionSlide.ordinal();
-	@Kroll.constant public static final int SWIPE_DUAL_FADE = TransitionHelper.Types.kTransitionSwipeDualFade.ordinal();
+    @Kroll.constant public static final int SWIPE_DUAL_FADE = TransitionHelper.Types.kTransitionSwipeDualFade.ordinal();
+    @Kroll.constant public static final int MODERN_PUSH = TransitionHelper.Types.kTransitionModernPush.ordinal();
 
 	@Kroll.constant public static final int LEFT_TO_RIGHT = TransitionHelper.SubTypes.kLeftToRight.ordinal();
 	@Kroll.constant public static final int RIGHT_TO_LEFT = TransitionHelper.SubTypes.kRightToLeft.ordinal();
