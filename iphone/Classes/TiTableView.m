e84d68b0dcdd8f6656e7542fed9856b39954b51a
Martin Guillon <martin.guillon@akylas.fr>
2014-07-21 18:49:43 +0200

    it seems to be fixed

--- iphone/Classes/TiTableView.m
+++ iphone/Classes/TiTableView.m
@@ -37,17 +37,17 @@
 
 - (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated
 {
-    if (IOS_7) {
-        //we have to delay it on ios7 :s
-        double delayInSeconds = 0.01;
-        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-            [super setContentOffset:contentOffset animated:animated];
-        });
-    }
-    else {
+//    if (IOS_7) {
+//        //we have to delay it on ios7 :s
+//        double delayInSeconds = 0.01;
+//        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+//        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+//            [super setContentOffset:contentOffset animated:animated];
+//        });
+//    }
+//    else {
         [super setContentOffset:contentOffset animated:animated];
-    }
+//    }
 }
 
 
