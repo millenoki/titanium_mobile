fe0ccb06e85aab111f6d01a5de6af508cb56d76c
Martin Guillon <martin.guillon@akylas.fr>
2014-11-16 14:57:16 +0100

    new method

--- android/modules/media/src/java/ti/modules/titanium/media/audio/RemoteControlClientCompat.java
+++ android/modules/media/src/java/ti/modules/titanium/media/audio/RemoteControlClientCompat.java
@@ -314,6 +314,29 @@ public class RemoteControlClientCompat {
      *       {@link android.media.RemoteControlClient#PLAYSTATE_BUFFERING},
      *       {@link android.media.RemoteControlClient#PLAYSTATE_ERROR}.
      */
+    public void setPlaybackState(int state, long timeInMs, float playbackSpeed) {
+        if (sHasRemoteControlAPIs) {
+            try {
+                sRCCSetPlayStateMethod.invoke(mActualRemoteControlClient, state, timeInMs, playbackSpeed);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
+    /**
+     * Sets the current playback state.
+     * @param state The current playback state, one of the following values:
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_STOPPED},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_PAUSED},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_PLAYING},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_FAST_FORWARDING},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_REWINDING},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_SKIPPING_FORWARDS},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_SKIPPING_BACKWARDS},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_BUFFERING},
+     *       {@link android.media.RemoteControlClient#PLAYSTATE_ERROR}.
+     */
     public void setPlaybackState(int state) {
         if (sHasRemoteControlAPIs) {
             try {
