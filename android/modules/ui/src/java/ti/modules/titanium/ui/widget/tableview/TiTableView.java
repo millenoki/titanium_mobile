608d56ab082866ca97174eb5ce1dff8a4d84cf33
Martin Guillon <martin.guillon@akylas.fr>
2014-12-22 16:58:42 +0100

    cleanup

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/tableview/TiTableView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/tableview/TiTableView.java
@@ -258,6 +258,7 @@ public class TiTableView extends FrameLayout
 				if (item.className.equals(TableViewProxy.CLASSNAME_HEADERVIEW)) {
 					TiViewProxy vproxy = item.proxy;
 					TiUIView headerView = layoutHeaderOrFooter(vproxy);
+					TiUIHelper.removeViewFromSuperview(headerView.getOuterView().getParent());
 					v = new TiTableViewHeaderItem(proxy.getActivity(), headerView);
 					v.setClassName(TableViewProxy.CLASSNAME_HEADERVIEW);
 					return v;
