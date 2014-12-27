65a2f22f4432014910cdb14eb133f25510861e71
Martin Guillon <martin.guillon@akylas.fr>
2014-12-27 19:24:43 +0100

    fixed defines

--- iphone/Classes/TiAudioMusicPlayer.h
+++ iphone/Classes/TiAudioMusicPlayer.h
@@ -4,7 +4,7 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-#ifdef USE_TI_AUDIOMUSICPLAYER
+#ifdef USE_TI_AUDIO
 
 #import <Foundation/Foundation.h>
 #import <MediaPlayer/MediaPlayer.h>
