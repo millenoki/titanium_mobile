1e0da2228a803bb4505f4b1adf49125bdec82b5b
Martin Guillon <martin.guillon@akylas.fr>
2015-01-05 17:26:10 +0100

    rewrote the default template

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiDefaultListViewTemplate.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiDefaultListViewTemplate.java
@@ -8,33 +8,16 @@ package ti.modules.titanium.ui.widget.listview;
 
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.titanium.TiC;
+import org.json.JSONException;
 
 public class TiDefaultListViewTemplate extends TiListViewTemplate {
 	private static final KrollDict TEMPLATE_DICT = createTemplateDict();
-
     private static KrollDict createTemplateDict() {
-    	KrollDict result = new KrollDict();
-    	KrollDict imageDict = new KrollDict();
-    	imageDict.put(TiC.PROPERTY_TYPE, "Ti.UI.ImageView");
-    	imageDict.put(TiC.PROPERTY_BIND_ID, "imageView");
-    	KrollDict props = new KrollDict();
-    	props.put(TiC.PROPERTY_TOUCH_ENABLED, false);
-    	props.put(TiC.PROPERTY_RIGHT, "25dp");
-    	props.put(TiC.PROPERTY_WIDTH, "15%");
-    	imageDict.put(TiC.PROPERTY_PROPERTIES, props);
-    	
-    	KrollDict labelDict = new KrollDict();
-    	labelDict.put(TiC.PROPERTY_TYPE, "Ti.UI.Label");
-    	labelDict.put(TiC.PROPERTY_BIND_ID, "titleView");
-    	props = new KrollDict();
-    	props.put(TiC.PROPERTY_TOUCH_ENABLED, false);
-    	props.put(TiC.PROPERTY_LEFT, "2dp");
-    	props.put(TiC.PROPERTY_WIDTH, "55%");
-    	props.put(TiC.PROPERTY_TEXT, "label");
-    	labelDict.put(TiC.PROPERTY_PROPERTIES, props);
-    	
-    	result.put(TiC.PROPERTY_CHILD_TEMPLATES, new Object[]{imageDict, labelDict});
-        return result;
+        try {
+            return new KrollDict("{properties:{layout:'horizontal'},childTemplates:[{type:'Ti.UI.ImageView',bindId:'imageView',touchEnabled:false,left:15,width:35',height:35},{properties:{layout:'vertical',touchEnabled:false,left:15,right:15},childTemplates:[{type:'Ti.UI.Label',bindId:'titleView',color:'white',font:{size:16},ellipsize:true,maxLines:1,height:'FILL',width:'FILL'},{type:'Ti.UI.Label',bindId:'subtitleView',color:'#7B7B7B',font:{size:15},ellipsize:true,maxLines:1,height:'FILL',width:'FILL', verticalAlign:'top'}]}]}");
+        } catch (JSONException e) {
+            return null;
+        }
     }
 
 	public TiDefaultListViewTemplate(String id) {
@@ -46,6 +29,9 @@ public class TiDefaultListViewTemplate extends TiListViewTemplate {
 	{
 		KrollDict result = super.prepareDataDict(dict);
 		KrollDict properties = result.containsKey(TiC.PROPERTY_PROPERTIES)?result.getKrollDict(TiC.PROPERTY_PROPERTIES):dict;
+		
+		boolean hasSubtitle = properties.containsKey(TiC.PROPERTY_SUBTITLE);
+		
 		if (properties.containsKey(TiC.PROPERTY_TITLE) || properties.containsKey(TiC.PROPERTY_FONT) || properties.containsKey(TiC.PROPERTY_COLOR))
 		{
 			KrollDict labelDict = result.getKrollDict("titleView");
@@ -54,6 +40,7 @@ public class TiDefaultListViewTemplate extends TiListViewTemplate {
 				labelDict = new KrollDict();
 				result.put("titleView", labelDict);
 			}
+			labelDict.put(TiC.PROPERTY_VERTICAL_ALIGN, hasSubtitle?"bottom":"center");
 			if (properties.containsKey(TiC.PROPERTY_TITLE)) {
 				labelDict.put(TiC.PROPERTY_TEXT, properties.get(TiC.PROPERTY_TITLE));
 			}
@@ -64,18 +51,36 @@ public class TiDefaultListViewTemplate extends TiListViewTemplate {
 				labelDict.put(TiC.PROPERTY_COLOR, properties.get(TiC.PROPERTY_COLOR));
 			}
 		}
-		if (properties.containsKey(TiC.PROPERTY_IMAGE))
-		{
-			KrollDict imageDict = result.getKrollDict("imageView");
-			if (imageDict == null)
-			{
-				imageDict = new KrollDict();
-				result.put("imageView", imageDict);
-			}
-			if (properties.containsKey(TiC.PROPERTY_IMAGE)) {
-				imageDict.put(TiC.PROPERTY_IMAGE, properties.get(TiC.PROPERTY_IMAGE));
-			}
-		}
+		KrollDict subDict = result.getKrollDict("subtitleView");
+        if (subDict == null)
+        {
+            subDict = new KrollDict();
+            result.put("subtitleView", subDict);
+        }
+		if (hasSubtitle)
+        {
+		    subDict.put(TiC.PROPERTY_TEXT, properties.get(TiC.PROPERTY_SUBTITLE));
+            subDict.put(TiC.PROPERTY_VISIBLE, true);
+            
+        } else {
+            subDict.put(TiC.PROPERTY_VISIBLE, false);
+        }
+		
+		subDict = result.getKrollDict("imageView");
+        if (subDict == null)
+        {
+            subDict = new KrollDict();
+            result.put("imageView", subDict);
+        }
+        if (properties.containsKey(TiC.PROPERTY_IMAGE))
+        {
+            subDict.put(TiC.PROPERTY_IMAGE, properties.get(TiC.PROPERTY_IMAGE));
+            subDict.put(TiC.PROPERTY_VISIBLE, true);
+            
+        } else {
+            subDict.put(TiC.PROPERTY_VISIBLE, false);
+        }
+		
 		return result;
 	}
 }
