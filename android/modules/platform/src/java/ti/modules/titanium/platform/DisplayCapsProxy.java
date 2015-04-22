e0a11ce43b57290910e08351b7dc4cdde6256351
Martin Guillon <martin.guillon@akylas.fr>
2015-04-22 11:26:42 +0200

    get real platform width and height

--- android/modules/platform/src/java/ti/modules/titanium/platform/DisplayCapsProxy.java
+++ android/modules/platform/src/java/ti/modules/titanium/platform/DisplayCapsProxy.java
@@ -7,10 +7,12 @@
 package ti.modules.titanium.platform;
 
 import java.lang.ref.SoftReference;
+import java.lang.reflect.Method;
 
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.TiContext;
 
 import android.util.DisplayMetrics;
@@ -19,18 +21,54 @@ import android.view.Display;
 @Kroll.proxy(parentModule=PlatformModule.class)
 public class DisplayCapsProxy extends KrollProxy
 {
-	private final DisplayMetrics dm;
+	private DisplayMetrics dm = null;
+	private boolean sizeSet = false;
 	private SoftReference<Display> softDisplay;
+	int realWidth;
+    int realHeight;
 
 	public DisplayCapsProxy()
 	{
 		super();
-		dm = new DisplayMetrics();
 	}
 
 	public DisplayCapsProxy(TiContext tiContext)
 	{
 		this();
+	}
+	
+	@SuppressWarnings("deprecation")
+    private void querySize() {
+	    if (sizeSet == false) {
+	        if (TiC.JELLY_BEAN_MR1_OR_GREATER){
+	            //new pleasant way to get real metrics
+	            DisplayMetrics realMetrics = new DisplayMetrics();
+	            getDisplay().getRealMetrics(realMetrics);
+	            realWidth = realMetrics.widthPixels;
+	            realHeight = realMetrics.heightPixels;
+
+	        } else if (TiC.ICS_OR_GREATER) {
+	            //reflection for this weird in-between time
+                Display display = getDisplay();
+	            try {
+	                Method mGetRawH = Display.class.getMethod("getRawHeight");
+	                Method mGetRawW = Display.class.getMethod("getRawWidth");
+	                realWidth = (Integer) mGetRawW.invoke(display);
+	                realHeight = (Integer) mGetRawH.invoke(display);
+	            } catch (Exception e) {
+	                //this may not be 100% accurate, but it's all we've got
+	                realWidth = display.getWidth();
+	                realHeight = display.getHeight();
+	            }
+
+	        } else {
+                Display display = getDisplay();
+	            //This should be close, as lower API devices should not have window navigation bars
+	            realWidth = display.getWidth();
+	            realHeight = display.getHeight();
+	        }
+	        sizeSet = true;
+	    }
 	}
 
 	private Display getDisplay() {
@@ -41,21 +79,25 @@ public class DisplayCapsProxy extends KrollProxy
 		}
 		return softDisplay.get();
 	}
+	
+	private DisplayMetrics getDisplayMetrics() {
+	    if (dm == null) {
+	        dm = new DisplayMetrics();
+	        getDisplay().getMetrics(dm);
+	    }
+	    return dm;
+	}
 
 	@Kroll.getProperty @Kroll.method
 	public int getPlatformWidth() {
-		synchronized(dm) {
-			getDisplay().getMetrics(dm);
-			return dm.widthPixels;
-		}
+	    querySize();
+	    return realWidth;
 	}
 
 	@Kroll.getProperty @Kroll.method
 	public int getPlatformHeight() {
-		synchronized(dm) {
-			getDisplay().getMetrics(dm);
-			return dm.heightPixels;
-		}
+	    querySize();
+        return realHeight;
 	}
 
 	@Kroll.getProperty @Kroll.method
@@ -65,26 +107,17 @@ public class DisplayCapsProxy extends KrollProxy
 
 	@Kroll.getProperty @Kroll.method
 	public float getDpi() {
-		synchronized(dm) {
-			getDisplay().getMetrics(dm);
-			return dm.densityDpi;
-		}
+		return getDisplayMetrics().densityDpi;
 	}
 
 	@Kroll.getProperty @Kroll.method
 	public float getXdpi() {
-		synchronized(dm) {
-			getDisplay().getMetrics(dm);
-			return dm.xdpi;
-		}
+		return getDisplayMetrics().xdpi;
 	}
 
 	@Kroll.getProperty @Kroll.method
 	public float getYdpi() {
-		synchronized(dm) {
-			getDisplay().getMetrics(dm);
-			return dm.ydpi;
-		}
+		return getDisplayMetrics().ydpi;
 	}
 
 	@Kroll.getProperty @Kroll.method
