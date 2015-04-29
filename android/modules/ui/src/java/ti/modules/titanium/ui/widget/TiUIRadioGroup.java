38860ff31d8f6a87d8dcf8186a8158605954e552
Martin Guillon <martin.guillon@akylas.fr>
2015-04-29 18:19:06 +0200

    support font and more

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIRadioGroup.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIRadioGroup.java
@@ -1,14 +1,18 @@
 package ti.modules.titanium.ui.widget;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.util.TiConvert;
+import org.appcelerator.titanium.util.TiUIHelper;
+import org.appcelerator.titanium.util.TiUIHelper.FontDesc;
 import org.appcelerator.titanium.view.TiUIView;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.support.v7.widget.AppCompatRadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
@@ -16,15 +20,33 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 public class TiUIRadioGroup extends TiUIView implements OnCheckedChangeListener {
     RadioGroup radioGroup;
     private boolean ignoreChangeEvent = false;
+    private FontDesc fontDesc = null;
+    private int selectedColor, color, disabledColor;
+    private boolean colorsSet = false;
 
     public TiUIRadioGroup(TiViewProxy proxy) {
         super(proxy);
         radioGroup = new RadioGroup(proxy.getActivity());
+        layoutParams.sizeOrFillWidthEnabled = true;
+        layoutParams.sizeOrFillHeightEnabled = true;
         radioGroup.setOnCheckedChangeListener(this);
+        setNativeView(radioGroup);
     }
     
+    protected static final ArrayList<String> KEY_SEQUENCE;
+    static{
+      ArrayList<String> tmp = new ArrayList<String>();
+      tmp.add(TiC.PROPERTY_COLOR);
+      tmp.add(TiC.PROPERTY_FONT);
+      tmp.add(TiC.PROPERTY_BUTTONS);
+      KEY_SEQUENCE = tmp;
+    }
+    @Override
+    protected ArrayList<String> keySequence() {
+        return KEY_SEQUENCE;
+    }
     
-    private AppCompatRadioButton createButton(final Context context, Object value) {
+    private AppCompatRadioButton createButton(final Context context, Object value, final ColorStateList colorStateList) {
         final AppCompatRadioButton result = new AppCompatRadioButton(context);
         if (value instanceof HashMap) {
             HashMap options = (HashMap) value;
@@ -32,6 +54,13 @@ public class TiUIRadioGroup extends TiUIView implements OnCheckedChangeListener 
         } else {
             result.setText(TiConvert.toString(value));
         }
+        if (fontDesc != null) {
+            TiUIHelper.styleText(result, fontDesc);
+        }
+        if (colorStateList != null) {
+            result.setTextColor(colorStateList);
+        }
+        
         return result;
     }
     
@@ -39,10 +68,27 @@ public class TiUIRadioGroup extends TiUIView implements OnCheckedChangeListener 
         if (!(value instanceof Object[])) {
             return;
         }
+        ColorStateList colorStateList = null;
         radioGroup.removeAllViews();
+        if (colorsSet) {
+            int[][] states = new int[][] {
+                    TiUIHelper.BACKGROUND_DISABLED_STATE, // disabled
+                    TiUIHelper.BACKGROUND_SELECTED_STATE, // pressed
+                    TiUIHelper.BACKGROUND_FOCUSED_STATE,  // pressed
+                    TiUIHelper.BACKGROUND_CHECKED_STATE,  // pressed
+                    new int [] {android.R.attr.state_pressed},  // pressed
+                    new int [] {android.R.attr.state_focused},  // pressed
+                    new int [] {}
+                };
+
+            colorStateList = new ColorStateList(
+                states,
+                new int[] {disabledColor, selectedColor, selectedColor, selectedColor, selectedColor, selectedColor, color}
+            );
+        }
         Object[] array = (Object[]) value;
         for (int i = 0; i < array.length; i++) {
-            AppCompatRadioButton button = createButton(context, array[i]);
+            AppCompatRadioButton button = createButton(context, array[i], colorStateList);
             if (button != null) {
                 button.setId(i);
                 radioGroup.addView(button);
@@ -70,6 +116,26 @@ public class TiUIRadioGroup extends TiUIView implements OnCheckedChangeListener 
         case TiC.PROPERTY_VALUE:
             ignoreChangeEvent = !changedProperty;
             check(TiConvert.toInt(newValue, -1));
+            break;
+        case TiC.PROPERTY_FONT:
+            fontDesc = TiUIHelper.getFontStyle(getContext(),
+                    TiConvert.toKrollDict(newValue));   
+            break;
+        case TiC.PROPERTY_COLOR:
+            if (!colorsSet) {
+                final AppCompatRadioButton button = new AppCompatRadioButton(
+                        proxy.getActivity());
+                color = disabledColor = selectedColor = button
+                        .getCurrentTextColor();
+                colorsSet = true;
+            }
+            color = selectedColor = disabledColor = color = TiConvert.toColor(newValue, this.color);
+            break;
+        case TiC.PROPERTY_SELECTED_COLOR:
+            selectedColor = TiConvert.toColor(newValue, this.selectedColor);
+            break;
+        case TiC.PROPERTY_DISABLED_COLOR:
+            disabledColor = TiConvert.toColor(newValue, this.disabledColor);
             break;
         case TiC.PROPERTY_BUTTONS:
             processButtons(proxy.getActivity(), newValue);
