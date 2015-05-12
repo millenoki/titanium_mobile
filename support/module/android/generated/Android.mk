b8718c41fb59bfd4b4468074f2f253d88b5ab490
Martin Guillon <martin.guillon@akylas.fr>
2015-05-12 16:18:24 +0200

    updated build system for modules to build with ndk r10d

--- support/module/android/generated/Android.mk
+++ support/module/android/generated/Android.mk
@@ -4,9 +4,14 @@ THIS_DIR := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
-THIS_DIR = $(LOCAL_PATH)
+LOCAL_MODULE := krollv8
+LOCAL_SRC_FILES := ../v8/libs/$(TARGET_ARCH_ABI)/libkroll-v8.so
+LOCAL_EXPORT_C_INCLUDES := v8/include
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := @MODULE_ID@
-LOCAL_CFLAGS := -g "-I$(TI_MOBILE_SDK)/android/native/include"
 
 # https://jira.appcelerator.org/browse/TIMOB-15263
 LOCAL_DISABLE_FORMAT_STRING_CHECKS=true
@@ -18,7 +23,7 @@ LOCAL_CFLAGS += -Wno-conversion-null
 # cf https://groups.google.com/forum/?fromgroups=#!topic/android-ndk/Q8ajOD37LR0
 LOCAL_CFLAGS += -Wno-psabi
 
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -ldl -llog -L$(TARGET_OUT) "-L$(TI_MOBILE_SDK)/android/native/libs/$(TARGET_ARCH_ABI)" -lkroll-v8
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -ldl -llog -L$(TARGET_OUT)
 
 GEN_DIR := $(realpath .)
 GEN_JNI_DIR := $(GEN_DIR)/jni
@@ -27,14 +32,16 @@ ABS_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp)
 BOOTSTRAP_CPP := $(wildcard $(LOCAL_PATH)/../*Bootstrap.cpp)
 
 LOCAL_SRC_FILES := $(patsubst $(LOCAL_PATH)/%,%,$(ABS_SRC_FILES)) \
-	$(patsubst $(LOCAL_PATH)/%,%,$(BOOTSTRAP_CPP))
+    $(patsubst $(LOCAL_PATH)/%,%,$(BOOTSTRAP_CPP))
+
+LOCAL_SHARED_LIBRARIES := krollv8
 
 $(BOOTSTRAP_CPP): $(GEN_DIR)/KrollGeneratedBindings.cpp $(GEN_DIR)/BootstrapJS.cpp
 
 $(GEN_DIR)/KrollGeneratedBindings.cpp:
-	gperf -L C++ -E -t "$(GEN_DIR)/KrollGeneratedBindings.gperf" > "$(GEN_DIR)/KrollGeneratedBindings.cpp"
+    gperf -L C++ -E -t "$(GEN_DIR)/KrollGeneratedBindings.gperf" > "$(GEN_DIR)/KrollGeneratedBindings.cpp"
 
 $(GEN_DIR)/BootstrapJS.cpp:
-	"$(PYTHON)" "$(TI_MOBILE_SDK)/module/android/js2c.py" "$(GEN_DIR)/BootstrapJS.cpp" "$(GEN_DIR)/bootstrap.js"
+    "$(PYTHON)" "$(TI_MOBILE_SDK)/module/android/js2c.py" "$(GEN_DIR)/BootstrapJS.cpp" "$(GEN_DIR)/bootstrap.js"
 
 include $(BUILD_SHARED_LIBRARY)
