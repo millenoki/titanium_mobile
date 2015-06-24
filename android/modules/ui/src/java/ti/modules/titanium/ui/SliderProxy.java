465fe330d4db9d4aeeb272c2c4969d9ea404bb92
Martin Guillon <martin.guillon@akylas.fr>
2015-06-24 15:21:52 +0200

    needs default background because of appcompat bug

--- android/modules/ui/src/java/ti/modules/titanium/ui/SliderProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/SliderProxy.java
@@ -25,6 +25,9 @@ public class SliderProxy extends ViewProxy
 	public SliderProxy()
 	{
 		super();
+		//there seems to be a bug with the latest appcompat. Without a background the
+        //switch is not showing
+        defaultValues.put(TiC.PROPERTY_BACKGROUND_COLOR, "transparent");
 	}
 
 	public SliderProxy(TiContext tiContext)
