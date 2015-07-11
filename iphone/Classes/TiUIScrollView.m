d4df3e2e4864e745bc36877d29270b11af36d866
Martin Guillon <martin.guillon@akylas.fr>
2015-07-11 17:49:48 +0200

    support point when setting zoomSCale

--- iphone/Classes/TiUIScrollView.m
+++ iphone/Classes/TiUIScrollView.m
@@ -357,7 +357,12 @@
 {
 	CGFloat scale = [TiUtils floatValue:value def:1.0];
 	BOOL animated = [TiUtils boolValue:@"animated" properties:property def:NO];
-	[[self scrollView] setZoomScale:scale animated:animated];
+    if ([property valueForKey:@"point"]) {
+        [self zoomToPoint:[TiUtils pointValue:@"point" properties:property] withScale:scale animated:animated];
+    } else {
+        [[self scrollView] setZoomScale:scale animated:animated];
+    }
+    
 	scale = [[self scrollView] zoomScale]; //Why are we doing this? Because of minZoomScale or maxZoomScale.
 	if ([self.proxy _hasListeners:@"scale"])
 	{
@@ -366,6 +371,35 @@
 											nil]];
 	}
 }
+
+- (void)zoomToPoint:(CGPoint)zoomPoint withScale: (CGFloat)scale animated: (BOOL)animated
+{
+    [self scrollView];
+    //Normalize current content size back to content scale of 1.0f
+    CGSize contentSize;
+    contentSize.width = (scrollView.contentSize.width / scrollView.zoomScale);
+    contentSize.height = (scrollView.contentSize.height / scrollView.zoomScale);
+    
+    //translate the zoom point to relative to the content rect
+    zoomPoint.x = (zoomPoint.x / self.bounds.size.width) * contentSize.width;
+    zoomPoint.y = (zoomPoint.y / self.bounds.size.height) * contentSize.height;
+    
+    //derive the size of the region to zoom to
+    CGSize zoomSize;
+    zoomSize.width = self.bounds.size.width / scale;
+    zoomSize.height = self.bounds.size.height / scale;
+    
+    //offset the zoom rect so the actual zoom point is in the middle of the rectangle
+    CGRect zoomRect;
+    zoomRect.origin.x = zoomPoint.x - zoomSize.width / 2.0f;
+    zoomRect.origin.y = zoomPoint.y - zoomSize.height / 2.0f;
+    zoomRect.size.width = zoomSize.width;
+    zoomRect.size.height = zoomSize.height;
+    
+    //apply the resize
+    [scrollView zoomToRect: zoomRect animated: animated];
+}
+
 
 -(void)setMaxZoomScale_:(id)args
 {
