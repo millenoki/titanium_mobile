9498406bca3931f5c487635b1296861df88cc305
Martin Guillon <martin.guillon@akylas.fr>
2015-07-22 18:09:00 +0200

    cleanup

--- iphone/Classes/TiTableView.m
+++ iphone/Classes/TiTableView.m
@@ -34,26 +34,26 @@
     }
     return self;
 }
-- (void)setContentOffset:(CGPoint)contentOffset
-{
-    [super setContentOffset:contentOffset];
-}
-
-- (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated
-{
-//    if (IOS_7) {
-//        //we have to delay it on ios7 :s
-//        double delayInSeconds = 0.01;
-//        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-//        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//            [super setContentOffset:contentOffset animated:animated];
-//        });
-//    }
-//    else {
-        [super setContentOffset:contentOffset animated:animated];
-//    }
-}
-
+//- (void)setContentOffset:(CGPoint)contentOffset
+//{
+//    [super setContentOffset:contentOffset];
+//}
+//
+//- (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated
+//{
+////    if (IOS_7) {
+////        //we have to delay it on ios7 :s
+////        double delayInSeconds = 0.01;
+////        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+////        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+////            [super setContentOffset:contentOffset animated:animated];
+////        });
+////    }
+////    else {
+//        [super setContentOffset:contentOffset animated:animated];
+////    }
+//}
+//
 
 -(BOOL)shouldHighlightCurrentListItem {
     return _shouldHighlightCurrentItem;
