029879fbbe1ec3a2ece9763b803a20b6a8477c7a
Martin Guillon <martin.guillon@akylas.fr>
2015-10-26 08:30:32 +0100

    allow properties change

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/abslistview/AbsListViewProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/abslistview/AbsListViewProxy.java
@@ -24,7 +24,6 @@ import org.appcelerator.titanium.util.TiConvert;
 import org.appcelerator.titanium.view.TiUIView;
 
 import ti.modules.titanium.ui.UIModule;
-import ti.modules.titanium.ui.widget.listview.TiListView;
 import android.app.Activity;
 import android.os.Handler;
 import android.os.Message;
@@ -39,6 +38,8 @@ import android.os.Message;
 	TiC.PROPERTY_SEPARATOR_COLOR,
 	TiC.PROPERTY_SEARCH_TEXT,
     TiC.PROPERTY_SEARCH_VIEW,
+    TiC.PROPERTY_HEADER_VIEW,
+    TiC.PROPERTY_FOOTER_VIEW,
     TiC.PROPERTY_SEARCH_VIEW_EXTERNAL,
 	TiC.PROPERTY_CASE_INSENSITIVE_SEARCH,
 	TiC.PROPERTY_HEADER_DIVIDERS_ENABLED,
