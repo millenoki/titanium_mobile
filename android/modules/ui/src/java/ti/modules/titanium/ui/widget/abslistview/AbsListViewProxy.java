b303b262869b99b6a72422b60515f2a0bb234237
Martin Guillon <martin.guillon@akylas.fr>
2015-12-20 17:23:20 +0100

    remove default selected background. Use selector instead

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/abslistview/AbsListViewProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/abslistview/AbsListViewProxy.java
@@ -106,7 +106,7 @@ public abstract class AbsListViewProxy extends TiViewProxy {
 	    defaultValues.put(TiC.PROPERTY_DEFAULT_ITEM_TEMPLATE, UIModule.LIST_ITEM_TEMPLATE_DEFAULT);
         defaultValues.put(TiC.PROPERTY_CASE_INSENSITIVE_SEARCH, true);
         defaultValues.put(TiC.PROPERTY_ROW_HEIGHT, 50);
-        defaultValues.put(TiC.PROPERTY_SELECTED_BACKGROUND_COLOR, "#474747");
+//        defaultValues.put(TiC.PROPERTY_SELECTED_BACKGROUND_COLOR, "#474747");
 		super.handleCreationDict(options);
 		//Adding sections to preload sections, so we can handle appendSections/insertSection
 		//accordingly if user call these before TiListView is instantiated.
