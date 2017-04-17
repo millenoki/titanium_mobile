3b303c6bf3655fb15dee8d9167801434d6974dee
Martin Guillon <martin.guillon@akylas.fr>
2017-04-17 11:54:09 +0200

    prevent negative size

--- android/titanium/src/java/org/appcelerator/titanium/view/TiCompositeLayout.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiCompositeLayout.java
@@ -446,7 +446,7 @@ public class TiCompositeLayout extends FreeLayout implements
 	                TiCompositeLayout.LayoutParams params = (TiCompositeLayout.LayoutParams) child
 	                        .getLayoutParams();
 	                final float weight = params.weight;
-	                final int childH = (int) ((h - maxHeight)*weight / (autoFillHeightTotalWeight - counter));
+	                final int childH = (int) (Math.max(0, h - maxHeight)*weight / (autoFillHeightTotalWeight - counter)), 0);
 	                counter += weight;
 
 	                final int widthPadding = getViewWidthPadding(child, params, this);
