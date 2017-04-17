85f9ce3d192adac5c64fe4c31b72be7268728636
Martin Guillon <martin.guillon@akylas.fr>
2017-04-17 11:54:57 +0200

    cleanup

--- android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
@@ -258,7 +258,6 @@ public abstract class TiBaseActivity extends AppCompatActivity
 
 	public void removeWindowFromStack(final TiWindowProxy proxy)
 	{
-	    boolean wasCurrentWindow = this.window == proxy;
 		proxy.onWindowFocusChange(false);
 
 		boolean isTopWindow = ( (!windowStack.isEmpty()) && (windowStack.peek() == proxy) ) ? true : false;
