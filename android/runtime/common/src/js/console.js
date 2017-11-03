28453af2605255ff8223466ff060e345d61f99b3
Martin Guillon <martin.guillon@akylas.fr>
2017-11-03 16:22:48 +0100

    time and timeEnd

--- android/runtime/common/src/js/console.js
+++ android/runtime/common/src/js/console.js
@@ -1,48 +1,71 @@
 /**
- * Appcelerator Titanium Mobile
- * Copyright (c) 2012-2013 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License
+ * Appcelerator Titanium Mobile Copyright (c) 2012-2013 by Appcelerator, Inc.
+ * All Rights Reserved. Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
- 
+
+exports._times = new Map()
+
+exports.time = function(label) {
+	exports._times.set(label, Date.now());
+};
+
+exports.timeEnd = function(label) {
+	var time = exports._times.get(label);
+	if (!time) {
+		throw new Error('No such label: ' + label);
+	}
+	var duration = Date.now() - time;
+	exports.log(label + ':', duration + 'ms');
+};
+
 function join(args) {
 	// Handle null / undefined args up front since we can't slice them
 	if (typeof args === "undefined") {
 		return "undefined";
-	}
-	else if (args === null) {
+	} else if (args === null) {
 		return "null";
 	}
-	
-	return [].concat(Array.prototype.slice.call(args)).map(function (arg) {
-		if (typeof arg === "undefined") {
-			return "undefined";
-		}
-		
-		return (arg === null) ? 
-			"null" : 
-			( (typeof arg === "object") ? 
-				( arg.hasOwnProperty('toString') ? arg.toString() : JSON.stringify(arg) ) :
-				arg );
-	}).join(' ');
+
+	return [].concat(Array.prototype.slice.call(args)).map(
+			function(arg) {
+				if (typeof arg === "undefined") {
+					return "undefined";
+				}
+
+				return (arg === null) ? "null"
+						: ((typeof arg === "object") ? (arg
+								.hasOwnProperty('toString') ? arg.toString()
+								: JSON.stringify(arg)) : arg);
+			}).join(' ');
 }
 
 exports.log = function() {
- 	Titanium.API.info(join(arguments));
- }
- 
- exports.info = function() {
- 	Titanium.API.info(join(arguments));
- }
- 
- exports.warn = function() {
- 	Titanium.API.warn(join(arguments));
- }
- 
- exports.error = function() {
- 	Titanium.API.error(join(arguments));
- }
- 
- exports.debug = function() {
- 	Titanium.API.debug(join(arguments));
- }
+	Titanium.API.info(join(arguments));
+}
+
+exports.info = function() {
+	Titanium.API.info(join(arguments));
+}
+
+exports.warn = function() {
+	Titanium.API.warn(join(arguments));
+}
+
+exports.error = function() {
+	Titanium.API.error(join(arguments));
+}
+
+exports.debug = function() {
+	Titanium.API.debug(join(arguments));
+}
+
+exports.trace = function() {
+	// TODO probably can to do this better with V8's debug object once that is
+	// exposed.
+	var err = new Error();
+	err.name = 'Trace';
+	err.message = util.format.apply(this, arguments);
+	Error.captureStackTrace(err, trace);
+	exports.error(err.stack);
+};
--- iphone/Classes/TiConsole.m
+++ iphone/Classes/TiConsole.m
@@ -8,6 +8,17 @@
 #import "TiConsole.h"
 
 @implementation TiConsole
+{
+    NSMutableDictionary* _times;
+}
+
+
+- (void)dealloc
+{
+    RELEASE_TO_NIL(_times);
+    [super dealloc];
+}
+
 
 - (void)log:(NSArray *)args withSeverity:(NSString *)severity
 {
@@ -42,5 +53,26 @@
 - (void)debug:(NSArray *)args
 {
   [self log:args withSeverity:@"debug"];
+}
+
+
+- (void)time:(id)args
+{
+    ENSURE_SINGLE_ARG(args, NSString)
+    if (!_times) {
+        _times = [[NSMutableDictionary alloc] init];
+    }
+    [_times setObject:@([[NSDate date] timeIntervalSince1970]*1000) forKey:args];
+}
+
+- (void)timeEnd:(id)args
+{
+    ENSURE_SINGLE_ARG(args, NSString)
+    NSNumber* time = [_times objectForKey:args];
+    if (!time) {
+        [self throwException:[NSString stringWithFormat:@"No such label: %@", args] subreason:nil location:CODELOCATION];
+    }
+    long duration = [[NSDate date] timeIntervalSince1970]*1000 - time.integerValue;
+    [self log:@[[NSString stringWithFormat:@"%@: %lims", args, (long)duration]] withSeverity:@"info"];
 }
 @end
